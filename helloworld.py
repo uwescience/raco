b07d9ad299cd0599daca248c5ea54e7ef2f7d23f
Bill Howe <billhowe@cs.washington.edu>
2013-03-13 11:02:16 -0700

    Reorganizing examples

--- helloworld.py
+++ helloworld.py
@@ -1,35 +1,25 @@
-from raco.language import PythonAlgebra, PseudoCodeAlgebra, CCAlgebra
-from raco.algebra import Select, Scan, Join, LogicalAlgebra
-from raco.compile import compile, optimize
-from raco.boolean import EQ, AND, OR, Attribute, StringLiteral, NumericLiteral
-import raco.scheme
-import raco.catalog
+from raco import RACompiler
+from raco.language import MyriaAlgebra
+from raco.algebra import LogicalAlgebra
 
-# declare the schema for each relation
-sch = raco.scheme.Scheme([("subject", int), ("predicate", int), ("object", int)])
+# A simple join
+query = """
+A(x,z) :- R(x,y), S(y,z)
+"""
 
-# Create a relation object.  We can add formats here as needed.
-trialdat = raco.catalog.ASCIIFile("trial.dat", sch)
+def comment(s):
+  print "/*\n%s\n*/" % str(s)
 
-# Now write the RA expression
+# Create a cmpiler object
+dlog = RACompiler()
 
-# Scan just takes a pointer to a relation object
-R = Scan(trialdat)
+# parse the query
+dlog.fromDatalog(query)
+print dlog.logicalplan
 
-# Select
-# EQ(x,y) means x=y, GT(x,y) means x>y, etc.
-sR = Select(EQ(Attribute("predicate"), NumericLiteral(1133564893)), R)
-sS = Select(EQ(Attribute("predicate"), NumericLiteral(77645021)), R)
-#sT = Select(EQ(Attribute("predicate"), NumericLiteral(77645021)), R)
-sT = Select(EQ(Attribute("object"), NumericLiteral(1018848684)), R)
+dlog.optimize(target=MyriaAlgebra, eliminate_common_subexpressions=False)
 
-# Join([(w,x),(y,z)], R, S) means "JOIN R, S ON (R.w = S.x AND R.y = S.z)" 
-sRsS = Join([("object","subject")], sR, sS)
-sRsSsT = Join([("object","subject")], sRsS, sT)
+# generate code in the target language
+code = dlog.compile()
+print code
 
-# optimize applies a set of rules to translate a source 
-# expression to a target expression
-result = optimize(sT, target=CCAlgebra, source=LogicalAlgebra) 
-
-# compile generates the linear code from the expression tree
-print compile(result)
