cfdc260964e3f290ec2dd31e00afc8189e2431fc
Bill Howe <billhowe@cs.washington.edu>
2013-09-05 00:03:34 -0700

    Relations now have a scheme method just like operators, rather than exposing a scheme member.

--- raco/catalog.py
+++ raco/catalog.py
@@ -3,12 +3,14 @@
 class Relation:
   def __init__(self, name, sch):
     self.name = name
-    self.scheme = sch
+    self._scheme = sch
 
   def __eq__(self, other):
     return self.name == other.name
     # and self.scheme == other.scheme
 
+  def scheme(self):
+    return self._scheme
 
 class FileRelation(Relation):
   pass
