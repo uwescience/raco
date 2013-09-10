0ed333bc464f54c685f209451ca234f42987dd1d
Daniel Halperin <dhalperi@cs.washington.edu>
2013-09-09 22:18:00 -0700

    runtests.py: make exit code indicate test success/failure
    For continuous integration we want the program running the tests to set its
    exit code nonzero if the tests fail. I followed the example
    [here](https://bugs.launchpad.net/myconnpy/+bug/459333).
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- runtests.py
+++ runtests.py
@@ -2,10 +2,12 @@
 
 from raco.tests import DatalogTest
 import raco.myrial.query_tests
+import sys
 import unittest
 
 test_cases = [DatalogTest, raco.myrial.query_tests.TestQueryFunctions]
 suites = [unittest.TestLoader().loadTestsFromTestCase(c) for c in test_cases]
 all_tests = unittest.TestSuite(suites)
 
-unittest.TextTestRunner(verbosity=2).run(all_tests)
+result = unittest.TextTestRunner(verbosity=2).run(all_tests)
+sys.exit(not result.wasSuccessful())
