220b38373862eda5a7050b847a3990f32b683694
Brandon Myers <bdmyers@cs.washington.edu>
2013-10-11 16:34:57 -0700

    logging in code gen

--- c++/cpp_test.py
+++ c++/cpp_test.py
@@ -8,34 +8,55 @@ import scan_code_ver2 as sc
 import raco.boolean
 from generateDot import generateDot
 
-#query = 'A(a1) :- R(a1,x),S(x,y),T(y,z),U(z,a1),100=z,y=50'
-#query = 'Triangle(x,y,z) :- R(x,y),S(y,z),T(z,x)'
-query = 'Triangle(x,y,z) :- edges(x,y),edges(y,z),edges(z,x),x<y,y<z'
-query = 'California(x,z) :- edges1(x,y1),edges1(y1,y2),edges2(y2,z)'
-#query = 'A(x,z) :- edges(x,y1),edges(y1,y2),edges(y2,z)'
-#query = 'we(a,c) :- edges(a,b),edges(b,c)'
-query = 'mutual(a,b) :- edges(a,b),edges(b,a)'
-query = 'symmetric(a,b) :- edges1(a,b),edges2(b,a)'
 
-print "query:", query, "\n"
+import logging
+LOG = logging.getLogger(__name__)
+import sys
 
-
-parsedprogram = parse(query)
-print "parsed:", parsedprogram, "\n"
-
-
-ra = parsedprogram.toRA()
-generateDot(ra,'diamond.dot')
-print "ra:", ra, "\n"
-
-physicalplan = optimize(ra, target=CCAlgebra, source=LogicalAlgebra)
-#print "physical plan:", physicalplan
-print 'args=',physicalplan[0][1].args
-print 'joinconditions=',physicalplan[0][1].joinconditions
-print 'leftconditions=',physicalplan[0][1].leftconditions
-print 'rightconditions=',physicalplan[0][1].rightconditions
-print 'final=',physicalplan[0][1].finalcondition
-tmp = sc.cpp_code(physicalplan,'california')
-print 'cpp_code result:',tmp
-tmp.gen_code()
-
+if __name__ == "__main__":
+  logging.basicConfig(level=logging.DEBUG)
+  
+  if len(sys.argv) > 1:
+      query = sys.argv[1]
+      set_sem = bool(sys.argv[2])
+      if (set_sem=='True'): set_sem = True
+      else: set_sem=False
+  else:
+      set_sem = False
+      #query = 'A(a1) :- R(a1,x),S(x,y),T(y,z),U(z,a1),100=z,y=50'
+      #query = 'Triangle(x,y,z) :- R(x,y),S(y,z),T(z,x)'
+      query = 'Triangle(x,y,z) :- edges(x,y),edges(y,z),edges(z,x),x<y,y<z'
+      query = 'California(x,z) :- edges1(x,y1),edges1(y1,y2),edges2(y2,z)'
+      #query = 'A(x,z) :- edges(x,y1),edges(y1,y2),edges(y2,z)'
+      #query = 'we(a,c) :- edges(a,b),edges(b,c)'
+      query = 'mutual(a,b) :- edges(a,b),edges(b,a)'
+      #query = 'symmetric(a,b) :- edges1(a,b),edges2(b,a)'
+      query = 'TwoPath(a,b,c) :- R(a,b),R(b,c)'
+  
+  import re
+  p = re.compile('[^(]*')
+  headname = p.match(query).group(0)
+  
+  print "query:", query, "\n"
+  
+  
+  parsedprogram = parse(query)
+  print "parsed:", parsedprogram, "\n"
+  
+  
+  ra = parsedprogram.toRA()
+  generateDot(ra,headname+'.dot')
+  print "ra:", ra, "\n"
+  
+  physicalplan = optimize(ra, target=CCAlgebra, source=LogicalAlgebra)
+  
+  LOG.info("physical: %s",physicalplan[0])
+  LOG.info('args=%s',physicalplan[0][1].args)
+  LOG.info('joinconditions=%s',physicalplan[0][1].joinconditions)
+  LOG.info('leftconditions=%s',physicalplan[0][1].leftconditions)
+  LOG.info('rightconditions=%s',physicalplan[0][1].rightconditions)
+  LOG.info('final=%s',physicalplan[0][1].finalcondition)
+  tmp = sc.cpp_code(physicalplan,headname,dis=set_sem)
+  LOG.info('cpp_code obj:%s',tmp)
+  tmp.gen_code()
+  
