2981aedc9da7cc6217e7ed42475058193ebdf57d
Andrew Whitaker <whitaker@cs.washington.edu>
2013-10-10 22:57:58 -0700

    Add a catalog defintion for Myrial examples

---
+++ examples/catalog.py
@@ -0,0 +1,5 @@
+# Schemas corresponding to Myrial examples
+
+{
+    'public:adhoc:edges': [('src','int'), ('dst', 'int')]
+}
--- myrial.py
+++ myrial.py
@@ -4,9 +4,11 @@
 
 import raco.myrial.interpreter as interpreter
 import raco.myrial.parser as parser
+import raco.scheme
 from raco import algebra
 
 import argparse
+import os
 import sys
 
 def print_pretty_plan(plan, indent=0):
@@ -30,10 +32,29 @@ def parse_options(args):
     ns = parser.parse_args(args)
     return ns
 
+class FakeCatalog(object):
+    def __init__(self, catalog):
+        self.catalog = catalog
+
+    def get_scheme(self, relation_key):
+        return raco.Scheme(self.catalog[relation_key])
+
+    @classmethod
+    def load_from_file(cls, path):
+        with open(path) as fh:
+            return cls(eval(fh.read()))
+
 def main(args):
     opt = parse_options(args)
+
+    # Search for a catalog definition file
+    catalog_path = os.path.join(os.path.dirname(opt.file), 'catalog.py')
+    catalog = None
+    if os.path.exists(catalog_path):
+        catalog = FakeCatalog.load_from_file(catalog_path)
+
     _parser = parser.Parser()
-    processor = interpreter.StatementProcessor()
+    processor = interpreter.StatementProcessor(catalog)
 
     with open(opt.file) as fh:
         statement_list = _parser.parse(fh.read())
