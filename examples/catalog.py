9ae1ee56056be390ef8df70a884283735c5779d2
Andrew Whitaker <whitaker@cs.washington.edu>
2013-10-10 22:59:32 -0700

    Add PageRank schema to examples catlog

--- examples/catalog.py
+++ examples/catalog.py
@@ -1,5 +1,6 @@
 # Schemas corresponding to Myrial examples
 
 {
-    'public:adhoc:edges': [('src','int'), ('dst', 'int')]
+    'public:adhoc:edges': [('src','int'), ('dst', 'int')],
+    'public:adhoc:vertices': [('id','int')]
 }
