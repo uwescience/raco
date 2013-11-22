abbb0db3df2736110cdcee2fdf5a40ae3bafcf25
Daniel Halperin <dhalperi@cs.washington.edu>
2013-11-22 09:05:49 -0800

    Printable: derive from object as new-style class

--- raco/utility.py
+++ raco/utility.py
@@ -4,7 +4,7 @@ Return a block of code as a string.
 def emit(*args):
     return "\n".join([str(x) for x in args if len(str(x)) > 0])
 
-class Printable:
+class Printable(object):
     def opname(self):
         return str(self.__class__.__name__)
 
