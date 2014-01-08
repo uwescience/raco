18f403f9329a0e8298fac01f52a295fc6e87d966
Brandon Myers <bdmyers@cs.washington.edu>
2014-01-08 14:30:02 -0800

    generate inserting into sqlite for testing

--- c_test_environment/generate_test_relations.py
+++ c_test_environment/generate_test_relations.py
@@ -6,11 +6,33 @@ def generate(basename, fields, tuples, datarange):
             for j in range(0,fields):
                 dat = random.randint(0, datarange)
                 f.write(str(dat))
-                f.write(' ')
+                if j<(fields-1):
+                    f.write(' ')
             f.write("\n")
+
+def importStatement(basename, fields):
+    template = """create table %(basename)s%(fields)s(%(fielddcls)s);
+.separator " "
+.import %(basename)s%(fields)s %(basename)s%(fields)s
+    
+"""
+
+    fielddcls = ""
+    names = ['a','b','c']
+    for i in range(0,fields):
+        fielddcls += names[i] + ' integer'
+        if i < fields-1:
+            fielddcls +=", "
+
+    text = template % locals()
+    return text
+
 
 if __name__ == "__main__":
     print 'generating'
-    for n in ['R','S','T']:
-        for nf in [1,2,3]:
-            generate(n, nf, 30, 10)
+    with open('importTestData.sql', 'w') as f:
+        for n in ['R','S','T']:
+            for nf in [1,2,3]:
+                generate(n, nf, 30, 10)
+                f.write(importStatement(n, nf))
+
