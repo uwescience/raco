f62a68fbd7cf2b7ccfcc269f5215fccabdd9e3bf
Brandon Myers <bdmyers@cs.washington.edu>
2014-02-04 19:43:47 -0800

    disable sp2bench that are not supported by compiler
    Two main problems
    Joins with $0 != $1
    condition flip (see issue #104)

--- examples/sp2bench/sp2bench_rdf.py
+++ examples/sp2bench/sp2bench_rdf.py
@@ -1,3 +1,6 @@
+import test_query
+from raco.language import CCAlgebra, MyriaAlgebra, GrappaAlgebra
+
 
 tr = "sp2bench_1m"
 queries = {}
@@ -6,7 +9,8 @@ queries['Q1'] = """A(yr) :- %(tr)s(journal, 'rdf:type', 'bench:Journal'),
                  %(tr)s(journal, 'dc:title', 'Journal 1 (1940)'),
                  %(tr)s(journal, 'dcterms:issued', yr)"""
 
-queries['Q2'] = """A(inproc, author, booktitle, title, proc, ee, page, url, yr, abstract) :- %(tr)s(inproc, 'rdf:type' 'bench:Inproceedings'),
+#"""A(inproc, author, booktitle, title, proc, ee, page, url, yr, abstract) :- %(tr)s(inproc, 'rdf:type', 'bench:Inproceedings'),
+queries['Q2'] = """A(inproc, author, booktitle, title, proc, ee, page, url, yr) :- %(tr)s(inproc, 'rdf:type', 'bench:Inproceedings'),
    %(tr)s(inproc, 'dc:creator', author),
    %(tr)s(inproc, 'bench:booktitle', booktitle),
    %(tr)s(inproc, 'dc:title', title),
@@ -22,7 +26,9 @@ queries['Q3a'] = """A(article) :- %(tr)s(article, 'rdf:type', 'bench:Article'),
                        %(tr)s(article, property, value),
                        property = 'swrc:pages'"""
 
-queries['Q4'] = """A(name1, name2) :- %(tr)s(article1, 'rdf:type', 'bench:Article'),
+#queries['Q4'] = """A(name1, name2) :- %(tr)s(article1, 'rdf:type', 'bench:Article'),
+#TODO: include q4 after issue #104 is addressed
+_ = """A(name1, name2) :- %(tr)s(article1, 'rdf:type', 'bench:Article'),
                            %(tr)s(article2, 'rdf:type', 'bench:Article'),
                            %(tr)s(article1, 'dc:creator', author1),
                            %(tr)s(author1, 'foaf:name', name1),
@@ -35,7 +41,9 @@ queries['Q4'] = """A(name1, name2) :- %(tr)s(article1, 'rdf:type', 'bench:Articl
 
 
 # syntactically join with equality; 
-queries['Q5a'] = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Article'),
+#TODO: include q5a after issue #104 is addressed
+#queries['Q5a'] = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Article'),
+_ = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Article'),
                             %(tr)s(article, 'dc:creator', person),
                             %(tr)s(inproc, 'rdf:type', 'bench:Inproceedings'),
                             %(tr)s(inproc, 'dc:creator', person2),
@@ -43,7 +51,9 @@ queries['Q5a'] = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Articl
                             %(tr)s(person2, 'foaf:name', name2),
                             name = name2"""
 # syntactically join with naming
-queries['Q5a'] = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Article'),
+#TODO: include q5b after issue #104 is addressed
+#queries['Q5b'] = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Article'),
+_ = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Article'),
                             %(tr)s(article, 'dc:creator', person),
                             %(tr)s(inproc, 'rdf:type', 'bench:Inproceedings'),
                             %(tr)s(inproc, 'dc:creator', person),
@@ -54,7 +64,9 @@ queries['Q5a'] = """A(person, name) :- %(tr)s(article, 'rdf:type', 'bench:Articl
 # TODO: Q7 requires double negation
 
 
-queries['Q8'] = """Erdoes(erdoes) :- %(tr)s(erdoes, 'rdf:type', 'foaf:Person'), 
+#TODO: enable Q8, after dealing with HashJoin( $0 != $7 ) type of cases
+#queries['Q8'] = """Erdoes(erdoes) :- %(tr)s(erdoes, 'rdf:type', 'foaf:Person'), 
+_ = """Erdoes(erdoes) :- %(tr)s(erdoes, 'rdf:type', 'foaf:Person'), 
                           %(tr)s(erdoes, 'foaf:name', "Paul Erdoes") 
         A(name) :- Erdoes(erdoes),
                    %(tr)s(doc, 'dc:creator', erdoes),
@@ -87,3 +99,8 @@ queries['Q10'] = """A(subj, pred) :- %(tr)s(subj, pred, 'person:Paul_Erdoes')"""
 
 queries['Q11'] = """A(ee) :- %(tr)s(publication, 'rdfs:seeAlso', ee)"""
 #TODO order by, limit, offset
+
+for name in queries:
+    querystr = queries[name] % locals()
+    test_query.testEmit(querystr, name, CCAlgebra)
+
