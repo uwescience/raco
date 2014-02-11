cc76f94c48409e266b3f08ac742da7ea40644319
Brandon Myers <bdmyers@cs.washington.edu>
2014-02-10 19:14:46 -0800

    add query to test flip bugs

--- examples/test_query.py
+++ examples/test_query.py
@@ -39,8 +39,16 @@ def testEmit(query, name, algebra):
 
 if __name__ == "__main__":
     queries = [
-            ("A(s1) :- T1(s1)", "scan"),#, "select s1 from T1"), 
-            ("A(s1) :- T1(s1), s1>10", "select"),#, "select s1 from T1 where s1>10" ),
+            ("""A(author) :- R(erdoes, 'rdf:type', 'foaf:Person'),
+                            R(doc, 10, erdoes),
+                            R(doc, 20, author),
+                            erdoes!=author""", "broken1"),
+            ("""A(author) :- R(erdoes, 'rdf:type', 'foaf:Person'),
+                            R(doc, 10, erdoes),
+                            R(doc, 20, author),
+                            author!=erdoes""", "broken2"),
+            ("A(s1) :- T1(s1)", "scan"),  # , "select s1 from T1"),
+            ("A(s1) :- T1(s1), s1>10", "select"),  # , "select s1 from T1 where s1>10" ),
             ("A(s1) :- T1(s1), s1>0, s1<10", "select_conjunction"),
             ("A(s1,s2) :- T2(s1,s2), s>10, s2>10", "two_var_select"),
             ("A(s1,o2) :- T3(s1,p1,o1), R3(o2,p1,o2)", "join"),
@@ -90,7 +98,7 @@ if __name__ == "__main__":
     algebra = CCAlgebra
     prefix = ""
     import sys
-    if sys.argv[1] ==  "grappa":
+    if len(sys.argv)>1 and sys.argv[1] ==  "grappa":
       algebra = GrappaAlgebra
       prefix = "grappa_"
 
