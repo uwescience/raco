19ca81d03c073121b6b42a07f00f8850d1fec4ce
Andrew Whitaker <whitaker@cs.washington.edu>
2014-02-17 11:51:33 -0800

    Fix control flow graph test

--- raco/myrial/cfg_test.py
+++ raco/myrial/cfg_test.py
@@ -6,10 +6,6 @@ import raco.myrial.myrial_test as myrial_test
 import raco.scheme as scheme
 import networkx as nx
 
-def graph_equals(g1, g2):
-  """Test for networkx graph equality."""
-  return g1.graph == g2.graph and g1.node == g2.node and g1.adj == g2.adj
-
 class CFGTest(myrial_test.MyrialTestCase):
     points_table = collections.Counter()
     points_schema = scheme.Scheme([('id', 'int'), ('x', 'float'),
@@ -43,11 +39,18 @@ class CFGTest(myrial_test.MyrialTestCase):
       expected.add_node(1, defs={"Big"}, uses={"Point"})
       expected.add_node(2, defs={"Continue"}, uses={"Big", "Point"})
       expected.add_node(3, defs=set(), uses={"Continue"})
-      expected.add_node(4, defs=set(), uses={"Big"})
+      expected.add_node(4, defs={'__OUTPUT0__'}, uses={"Big"})
 
       for i in range(4):
           expected.add_edge(i, i + 1)
       expected.add_edge(3, 1)
 
-      actual = self.processor.cfg
-      self.assertTrue(graph_equals(actual, expected))
+      actual = self.processor.cfg.graph
+
+      self.assertEquals(actual.adj, expected.adj)
+      self.assertEquals(len(actual), len(expected))
+
+      for n in expected:
+          self.assertIn(n, actual)
+          self.assertEquals(actual.node[n]['uses'], expected.node[n]['uses'])
+          self.assertEquals(actual.node[n]['defs'], expected.node[n]['defs'])
