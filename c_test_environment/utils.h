773f72c3f19c0086540902d7727a55bb14e746c0
Brandon Myers <bdmyers@cs.washington.edu>
2014-03-07 23:46:35 -0800

    more hash functions

--- c_test_environment/utils.h
+++ c_test_environment/utils.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <functional>
+
 uint64_t identity_hash( int64_t k ) {
   return k;
 }
+
+uint64_t linear_hash( int64_t k) {
+  return (73251599 * k + 110802387) % 98764321261;
+}
+
+static std::hash<int64_t> sh; 
+uint64_t std_hash( int64_t k ) {
+  return sh(k);
+}
+
 
 template <typename D, typename S1, typename S2>
 D combine(S1 s1, S2 s2) {
