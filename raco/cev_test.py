97d63c9b5e476333adf805c63298fe5128f386f7
Andrew Whitaker <whitaker@cs.washington.edu>
2014-03-21 13:47:02 -0700

    Unit test of column equivalence test class

---
+++ raco/cev_test.py
@@ -0,0 +1,21 @@
+import unittest
+import physprop
+
+
+class ColumnEquivalenceClassTest(unittest.TestCase):
+
+    def test_cev(self):
+        cev = physprop.ColumnEquivalenceClassSet(12)
+
+        for i in range(12):
+            self.assertEquals(cev.get_equivalent_columns(i), {i})
+
+        cev.merge(1, 2)
+        cev.merge(4, 3)
+        cev.merge(3, 2)
+
+        for i in range(1, 5):
+            self.assertEquals(cev.get_equivalent_columns(i), {1, 2, 3, 4})
+
+        for i in range(5, 12):
+            self.assertEquals(cev.get_equivalent_columns(i), {i})
