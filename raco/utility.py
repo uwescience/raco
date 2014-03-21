1ac17f3cb7bb538938332ea15bfd39213acdae5d
Andrew Whitaker <whitaker@cs.washington.edu>
2014-03-21 13:14:57 -0700

    Add common equality mixin
    style fixes

--- raco/utility.py
+++ raco/utility.py
@@ -15,3 +15,13 @@ class Printable(object):
 
     def __str__(self):
         return self.opname()
+
+
+class CommonEqualityMixin(object):
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+                and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
