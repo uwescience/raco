15df0935ab2f524a966569299cb794432a3568d0
Andrew Whitaker <whitaker@cs.washington.edu>
2014-03-25 12:51:59 -0700

    Test simple selection logic

--- raco/comm_test.py
+++ raco/comm_test.py
@@ -3,6 +3,7 @@ import unittest
 import scheme
 import relation_key
 from algebra import *
+from expression import *
 from comm import *
 from physprop import *
 
@@ -15,12 +16,22 @@ class CommunicationTests(unittest.TestCase):
                                     ("w", "int")])
         self.rel_key = relation_key.RelationKey.from_string("andrew:adhoc:ZZZ")
 
-    def __validate(self, op_out, cevs, how_partitioned):
+    def validate(self, op_in, cevs, how_partitioned):
+        def rec(op):
+            op.apply(rec)
+            return self.cv.visit(op)
+
+        op_out = rec(op_in)
         self.assertEquals(op_out.how_partitioned, how_partitioned)
         self.assertEquals(op_out.column_equivalences, cevs)
 
     def test_scan(self):
         op_in = Scan(self.rel_key, self.scheme)
-        op_out = self.cv.visit(op_in)
+        self.validate(op_in, ColumnEquivalenceClassSet(4), PARTITION_RANDOM)
 
-        self.__validate(op_out, ColumnEquivalenceClassSet(4), PARTITION_RANDOM)
+    def test_select(self):
+        scan = Scan(self.rel_key, self.scheme)
+        cond = GT(NamedAttributeRef("y"), NumericLiteral(1))
+        select = Select(condition=cond, input=scan)
+
+        self.validate(select, ColumnEquivalenceClassSet(4), PARTITION_RANDOM)
