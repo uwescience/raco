b0219e54e8356811d96d950fb17ea3f606023919
Andrew Whitaker <whitaker@cs.washington.edu>
2014-03-26 14:23:53 -0700

    Add seaflow examples to raco/examples

--- examples/catalog.py
+++ examples/catalog.py
@@ -7,5 +7,10 @@
     'public:adhoc:sc_points': [('v', 'float')],
     'public:adhoc:employee' : [('id', 'int'), ('dept_id', 'int'), ('name', 'string'),
                                 ('salary','int')],
-    'public:adhoc:departments' : [('id', 'int'), ('name','string')]
+    'public:adhoc:departments' : [('id', 'int'), ('name','string')],
+    'armbrustlab:seaflow:all_data' : [('Cruise', 'int'),
+                                      ('Day', 'int'),
+                                      ('File_Id', 'int'),
+                                      ('chl_small', 'float'),
+                                      ('pe', 'float')]
 }
