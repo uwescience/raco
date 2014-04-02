80238c6d0fd05e289da0249faaf1dc0ab2103894
Andrew Whitaker <whitaker@cs.washington.edu>
2014-04-02 16:16:01 -0700

    Add a unique ID to sigma clipping schema

--- raco/myrial/sigma_clipping_test.py
+++ raco/myrial/sigma_clipping_test.py
@@ -8,10 +8,10 @@ import raco.myrial.myrial_test as myrial_test
 class SigmaClippingTest(myrial_test.MyrialTestCase):
     points = [25.0, 27.2, 23.4, 25.1, 26.3, 24.9, 23.5, 22.7, 108.2,
               26.2, 25.3, 24.7, 25.0, 26.1, 22.8, 2.2, 24.8, 25.05, 25.15]
-    points_tuples = [tuple([x]) for x in points]
+    points_tuples = [(i, x) for i, x in enumerate(points)]
     points_table = collections.Counter(points_tuples)
 
-    points_schema = scheme.Scheme([('v', 'float')])
+    points_schema = scheme.Scheme([('id', 'int'), ('v', 'float')])
     points_key = "public:adhoc:sc_points"
 
     def setUp(self):
@@ -25,20 +25,17 @@ class SigmaClippingTest(myrial_test.MyrialTestCase):
         self.db.ingest("empty", collections.Counter(),
                        SigmaClippingTest.points_schema)
 
+    def run_it(self, query):
+        points = [(i, x) for i, x in self.points_tuples if x < 28 and x > 22]
+        expected = collections.Counter(points)
+        self.check_result(query, expected, skip_json=True)
+
     def test_v0(self):
         with open('examples/sigma-clipping-v0.myl') as fh:
             query = fh.read()
-
-        points = [x for x in self.points if x < 28 and x > 22]
-        expected = collections.Counter([tuple([x]) for x in points])
-
-        self.check_result(query, expected, skip_json=True)
+        self.run_it(query)
 
     def test_v2(self):
         with open('examples/sigma-clipping.myl') as fh:
             query = fh.read()
-
-        points = [x for x in self.points if x < 28 and x > 22]
-        expected = collections.Counter([tuple([x]) for x in points])
-
-        self.check_result(query, expected, skip_json=True)
+        self.run_it(query)
