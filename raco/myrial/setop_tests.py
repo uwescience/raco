1d649a868106a527bf37aafcf6a47a507ea94569
Andrew Whitaker <whitaker@cs.washington.edu>
2014-04-03 09:41:53 -0700

    Test of inline unionall

--- raco/myrial/setop_tests.py
+++ raco/myrial/setop_tests.py
@@ -53,6 +53,15 @@ class SetopTestFunctions(myrial_test.MyrialTestCase):
         expected = self.emp_table1 + self.emp_table2
         self.check_result(query, expected)
 
+    def test_unionall_inline(self):
+        query = """
+        out = SCAN(%s) + SCAN(%s);
+        STORE(out, OUTPUT);
+        """ % (self.emp_key1, self.emp_key2)
+
+        expected = self.emp_table1 + self.emp_table2
+        self.check_result(query, expected)
+
     def test_diff1(self):
         query = """
         out = DIFF(SCAN(%s), SCAN(%s));
