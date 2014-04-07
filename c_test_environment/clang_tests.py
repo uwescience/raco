709575c165dc44e0ef275982a364763a71ad7135
Brandon Myers <bdmyers@cs.washington.edu>
2014-04-07 14:25:42 -0700

    for now forced to nose skip tests with clang due to travis environment

--- c_test_environment/clang_tests.py
+++ c_test_environment/clang_tests.py
@@ -10,6 +10,8 @@ sys.path.append('./examples')
 from emitcode import emitCode
 from osutils import Chdir
 
+# skipping
+from nose.tools import nottest
 
 
 class ClangTest(unittest.TestCase):
@@ -23,16 +25,19 @@ class ClangTest(unittest.TestCase):
         if not os.path.isfile(testdbname()):
             generate_default()  
         
-
+    @nottest
     def test_scan(self):
         self.check("A(s1) :- T1(s1)", "scan")
 
+    @nottest
     def test_select(self):
         self.check("A(s1) :- T1(s1), s1>5", "select") 
 
+    @nottest
     def test_join(self):
         self.check("A(s1,o2) :- T3(s1,p1,o1), R3(o2,p1,o2)", "join")
             
+    @nottest
     def test_select_conjunction(self):
         self.check("A(s1) :- T1(s1), s1>0, s1<10", "select_conjunction"),
 
