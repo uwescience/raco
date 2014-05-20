22476992066ba086a3c27b248be3c491ed91cf3c
Shumo Chu <shumo.chu@gmail.com>
2014-05-20 00:11:55 -0700

    fix a bug in operator test.

--- raco/operator_test.py
+++ raco/operator_test.py
@@ -77,7 +77,7 @@ class OperatorTest(unittest.TestCase):
         from myrialang import compile_to_json, MyriaAlgebra
         from compile import optimize
         import json
-        json_string = json.dumps(compile_to_json("", None, optimize([(None, sapply)],  MyriaAlgebra(), LogicalAlgebra)))  # noqa
+        json_string = json.dumps(compile_to_json("", None, optimize([(None, sapply)], MyriaAlgebra(), LogicalAlgebra)))  # noqa
         assert json_string
 
     def test_cast_to_float(self):
