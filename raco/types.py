443a967ad556af9d848700db00f0098188808f82
Andrew Whitaker <whitaker@cs.washington.edu>
2014-05-28 12:49:11 -0700

    Create a registry of primitive types

---
+++ raco/types.py
@@ -0,0 +1,10 @@
+
+"""Names of primitive types understand by raco."""
+
+# Map from myrial token name to "official" type name.
+type_map = {
+    "STRING": "STRING_TYPE",
+    "INT": "LONG_TYPE",
+    "FLOAT": "FLOAT_TYPE",
+    "BOOLEAN": "BOOLEAN_TYPE"
+}
