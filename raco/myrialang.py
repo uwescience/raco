bc0b4585c09c81cff754f388af5d495a7b91858b
Andrew Whitaker <whitaker@cs.washington.edu>
2014-05-29 10:47:54 -0700

    Cleanup scheme_to_schema

--- raco/myrialang.py
+++ raco/myrialang.py
@@ -15,24 +15,10 @@ from raco.expression.aggregate import DecomposableAggregate
 
 
 def scheme_to_schema(s):
-    def convert_typestr(t):
-        if t.lower() in ['bool', 'boolean']:
-            return 'BOOLEAN_TYPE'
-        if t.lower() in ['float', 'double']:
-            return 'DOUBLE_TYPE'
-#    if t.lower() in ['float']:
-#      return 'DOUBLE_TYPE'
-#    if t.lower() in ['int', 'integer']:
-#      return 'INT_TYPE'
-        if t.lower() in ['int', 'integer', 'long']:
-            return 'LONG_TYPE'
-        if t.lower() in ['str', 'string']:
-            return 'STRING_TYPE'
-        return t
     if s:
         names, descrs = zip(*s.asdict.items())
         names = ["%s" % n for n in names]
-        types = [convert_typestr(r[1]) for r in descrs]
+        types = [r[1] for r in descrs]
     else:
         names = []
         types = []
