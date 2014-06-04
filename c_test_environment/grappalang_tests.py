9cdd1eac219f0ca2791cbc63aff6c94cbfb16878
Brandon Myers <bdmyers@cs.washington.edu>
2014-06-04 16:44:55 -0700

    factor out common test cases and create grappa tests

--- c_test_environment/grappalang_tests.py
+++ c_test_environment/grappalang_tests.py
@@ -1,9 +1,9 @@
 import unittest
 from testquery import checkquery
 from testquery import testdbname
-from testquery import ClangRunner
+from testquery import GrappalangRunner
 from generate_test_relations import generate_default
-from raco.language import CCAlgebra
+from raco.language import GrappaAlgebra
 from platform_tests import PlatformTest
 
 import sys
@@ -13,17 +13,17 @@ from emitcode import emitCode
 from osutils import Chdir
 
 
-class ClangTest(unittest.TestCase, PlatformTest):
+class GrappaTest(unittest.TestCase, PlatformTest):
     def check(self, query, name):
         chdir = Chdir("c_test_environment")
-        emitCode(query, name, CCAlgebra)
-        checkquery(name, ClangRunner())
+        emitCode(query, name, GrappaAlgebra)
+        checkquery(name, GrappalangRunner())
 
     def setUp(self):
         chdir = Chdir("c_test_environment")
         if not os.path.isfile(testdbname()):
-            generate_default()  
-        
+            generate_default()
+
 
 if __name__ == '__main__':
     unittest.main()
