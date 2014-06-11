e3e6af5e99dca5d9559f311a3d4f675782b77d10
Andrew Whitaker <whitaker@cs.washington.edu>
2014-06-11 11:23:59 -0700

    Add implicit column name test

--- raco/myrial/query_tests.py
+++ raco/myrial/query_tests.py
@@ -1626,6 +1626,18 @@ class TestQueryFunctions(myrial_test.MyrialTestCase):
 
         self.check_result(query, collections.Counter([(1,)]))
 
+    def test_implicit_column_names(self):
+        query = """
+        x = [1 as a, 2 as b];
+        y = [from x as x1, x as x2
+             emit $0, $1];
+        store(y, OUTPUT);"""
+
+        expected_scheme = scheme.Scheme([('a', types.LONG_TYPE),
+                                         ('b', types.LONG_TYPE)])
+        self.check_result(query, collections.Counter([(1, 2)]),
+                          scheme=expected_scheme)
+
     def test_duplicate_column_names(self):
         query = """
         x = [1 as a, 2 as b];
