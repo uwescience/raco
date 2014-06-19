8cd139e91202e72099e77d29e1843707fa6a9366
Daniel Halperin <dhalperi@cs.washington.edu>
2014-06-19 01:28:47 -0700

    catalog: use relation_key as the key
    rather than turning it back into a string.

--- raco/catalog.py
+++ raco/catalog.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from raco.relation_key import RelationKey
 
 
 class Relation(object):
@@ -42,14 +43,12 @@ class FakeCatalog(MyriaCatalog):
         # overwrite default sizes if necessary
         if child_sizes:
             for child, size in child_sizes.items():
-                self.cached["public:adhoc:{}".format(child)] = size
+                self.cached[RelationKey(child)] = size
 
     def get_num_servers(self):
         return self.num_servers
 
     def num_tuples(self, rel_key):
-        key = "{}:{}:{}".format(
-            rel_key.user, rel_key.program, rel_key.relation)
-        if key in self.cached:
-            return self.cached[key]
+        if rel_key in self.cached:
+            return self.cached[rel_key]
         return 10000
