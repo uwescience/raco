266e8be8b410434676415a066393dcdb12d20e26
Andrew Whitaker <whitaker@cs.washington.edu>
2014-06-18 21:19:26 -0700

    test cases for aql escaping

---
+++ raco/myrial/aql_tests.py
@@ -0,0 +1,32 @@
+"""Tests of AQL escaping."""
+
+from raco.myrial.myrial_test import MyrialTestCase
+from raco.algebra import *
+from collections import Counter
+from raco.scheme import Scheme
+
+class AqlTests(MyrialTestCase):
+    def setUp(self):
+        super(AqlTests, self).setUp()
+        self.db.ingest("public:adhoc:employees", Counter, Scheme())
+
+    def test_escaped_aql(self):
+        aql =  'SELECT * FROM TestArray'
+        query = "%%aql %s;" % aql
+        plan = self.get_plan(query, logical=True)
+        expected = Sequence([Exec(aql, "AQL")])
+
+        self.assertEquals(plan, expected)
+
+    def test_escaped_aql_with_myrial(self):
+        aql =  'SELECT * FROM TestArray'
+        query = """
+        %%aql %s;
+        X = SCAN(public:adhoc:employees);
+        STORE(X, OUTPUT);""" % aql
+
+        plan = self.get_plan(query, logical=True)
+
+        self.assertIsInstance(plan, Sequence)
+        self.assertIsInstance(plan.args[0], Exec)
+        self.assertIsInstance(plan.args[1], Store)
