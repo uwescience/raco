7879c976359e40f01d1d532bca8f77de0522c1e6
Andrew Whitaker <whitaker@cs.washington.edu>
2014-06-22 09:00:36 -0700

    unit test of federated optimization

---
+++ raco/bigdog/bigdog_test.py
@@ -0,0 +1,45 @@
+import unittest
+
+from raco.scheme import Scheme
+from raco.language import FederatedAlgebra
+from raco.algebra import *
+from raco.expression import expression as e
+from raco.expression import boolean
+from raco.compile import optimize
+from raco.types import FLOAT_TYPE, INT_TYPE
+from raco.relation_key import RelationKey
+from raco.federatedlang import *
+
+
+class BigdogTest(unittest.TestCase):
+    def test_optimize(self):
+        sch = Scheme([("y", FLOAT_TYPE), ("x", INT_TYPE)])
+
+        R = RelationKey.from_string("public:adhoc:X")
+
+        R1 = Scan(R, sch)
+        R2 = Scan(R, sch)
+
+        J = Join(boolean.EQ(e.UnnamedAttributeRef(0),
+                            e.UnnamedAttributeRef(3)), R1, R2)
+
+        emitters = [("z", e.PLUS(e.NamedAttributeRef("x"),
+                                 e.NamedAttributeRef("y"))),
+                    ("w", e.UnnamedAttributeRef(3))]
+
+        _apply = Apply(emitters, J)
+        myria_query = Store(RelationKey.from_string("public:adhoc:OUTPUT"),
+                            _apply)
+
+        sdb = None  # connect('http://vega.cs.washington.edu:5555')
+        aqlscheme = Scheme([("j", INT_TYPE)])
+        scidb_query = ExecScan("SELECT j FROM B WHERE j > 3 AND j < 7",
+                               languagetag="aql", connection=sdb,
+                               scheme=aqlscheme)
+
+        seq = Sequence([myria_query, scidb_query])
+        exprs = optimize(seq, target=FederatedAlgebra(), source=LogicalAlgebra)
+
+        self.assertIsInstance(exprs, Sequence)
+        self.assertIsInstance(exprs.args[0], RunMyria)
+        self.assertIsInstance(exprs.args[1], RunAQL)
