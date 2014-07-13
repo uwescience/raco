06fa441e2595854d3cd6b37e08e636627174f9b1
Andrew Whitaker <whitaker@cs.washington.edu>
2014-07-13 08:11:22 -0700

    Test fully connected join graph

--- raco/language/join_graph_test.py
+++ raco/language/join_graph_test.py
@@ -27,3 +27,13 @@ class JoinGraphTest(unittest.TestCase):
         for n1 in [0, 1, 2]:
             for n2 in [0, 1, 2]:
                 self.assertEquals(jg.get_edges(n1, n2), set())
+
+    def test_fully_connected(self):
+        jg = JoinGraph(3)
+        jg.add_edge(0, 2, "hello")
+        jg.add_edge(2, 0, "world")
+        jg.add_edge(2, 1, "goodbye")
+        jg.add_edge(0, 1, "oops")
+
+        jo = jg.choose_left_deep_join_order()
+        self.assertEquals(jo, [0, 1, 2])
