7315067d8601220a78df3dc9e0950eac83277a10
Andrew Whitaker <whitaker@cs.washington.edu>
2014-07-13 08:03:51 -0700

    style fix

--- raco/language/join_graph_test.py
+++ raco/language/join_graph_test.py
@@ -10,7 +10,7 @@ class JoinGraphTest(unittest.TestCase):
         jg.add_edge(2, 1, "goodbye")
 
         jo = jg.choose_left_deep_join_order()
-        self.assertIn(tuple(jo), {(0,2,1), (2,1,0), (2,0,1), (1,2,0)})
+        self.assertIn(tuple(jo), {(0, 2, 1), (2, 1, 0), (2, 0, 1), (1, 2, 0)})
 
         self.assertEquals(jg.get_edges(0, 2), {"hello", "world"})
         self.assertEquals(jg.get_edges(2, 0), {"hello", "world"})
