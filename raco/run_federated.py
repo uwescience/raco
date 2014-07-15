bda2dbc9230a75ec6836772da083a9d425f559d5
Andrew Whitaker <whitaker@cs.washington.edu>
2014-07-14 22:19:12 -0700

    Return a query status object from myria connection

--- raco/run_federated.py
+++ raco/run_federated.py
@@ -3,19 +3,21 @@ from raco.language import FederatedAlgebra
 from raco.algebra import LogicalAlgebra, Sequence
 from raco.federatedlang import *
 
-from scidbpy import connect
+#from scidbpy import connect
 
 def run(logical_plan, myria_conn):
     seq_op = optimize(logical_plan, target=FederatedAlgebra(),
                       source=LogicalAlgebra)
     assert isinstance(seq_op, Sequence)
 
+    outs = []
     for op in seq_op.args:
         if isinstance(op, RunAQL):
             print op.command
             sdb = connect(op.connection)
             sdb._execute_query(op.command)
         elif isinstance(op, RunMyria):
-            print op.command
-            qs = myria_conn.submit_query(op.command)
-            print qs
+            outs.append(myria_conn.submit_query(op.command))
+
+    if len(outs) > 0:
+        return outs[-1]  # XXX This is strange
