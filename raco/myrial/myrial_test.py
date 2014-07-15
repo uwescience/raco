0c062d0e5e1c8ad9ee50d1364d7819e5a8d1a80c
Brandon Myers <bdmyers@cs.washington.edu>
2014-07-15 15:00:48 -0700

    allow for more than just Myria compiler to use myrial_test

--- raco/myrial/myrial_test.py
+++ raco/myrial/myrial_test.py
@@ -3,6 +3,7 @@ import unittest
 
 from raco.language.myrialang import compile_to_json
 import raco.fakedb
+from raco.language.myrialang import MyriaLeftDeepTreeAlgebra
 import raco.myrial.interpreter as interpreter
 import raco.myrial.parser as parser
 import raco.viz
@@ -11,7 +12,9 @@ import raco.viz
 class MyrialTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db = raco.fakedb.FakeDatabase()
+        if not hasattr(self, 'db'):
+            self.db = raco.fakedb.FakeDatabase()
+
         self.parser = parser.Parser()
         self.processor = interpreter.StatementProcessor(self.db)
 
@@ -20,14 +23,15 @@ class MyrialTestCase(unittest.TestCase):
         statements = self.parser.parse(query)
         self.processor.evaluate(statements)
 
-    def get_plan(self, query, logical=False, multiway_join=False):
+    def get_plan(self, query, logical=False,
+                 target_alg=MyriaLeftDeepTreeAlgebra()):
         '''Get the MyriaL query plan for a query'''
         statements = self.parser.parse(query)
         self.processor.evaluate(statements)
         if logical:
             p = self.processor.get_logical_plan()
         else:
-            p = self.processor.get_physical_plan(multiway_join)
+            p = self.processor.get_physical_plan_for(target_alg)
         # verify that we can stringify p
         # TODO verify the string somehow?
         assert str(p)
@@ -40,9 +44,9 @@ class MyrialTestCase(unittest.TestCase):
         '''Get the logical plan for a MyriaL query'''
         return self.get_plan(query, logical=True)
 
-    def get_physical_plan(self, query, multiway_join=False):
+    def get_physical_plan(self, query, target_alg=MyriaLeftDeepTreeAlgebra()):
         '''Get the physical plan for a MyriaL query'''
-        return self.get_plan(query, logical=False, multiway_join=multiway_join)
+        return self.get_plan(query, logical=False, target_alg=target_alg)
 
     def execute_query(self, query, test_logical=False, skip_json=False,
                       output='OUTPUT'):
