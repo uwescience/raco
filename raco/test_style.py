8e4c58c84831b372335c58914ba6bc9a67b6f8ec
Daniel Halperin <dhalperi@cs.washington.edu>
2014-07-14 17:59:49 -0700

    test_style: add pylint and move output to test failure

--- raco/test_style.py
+++ raco/test_style.py
@@ -3,12 +3,22 @@ import sys
 import unittest
 
 
+def check_output_and_print_stderr(args):
+    """Run the specified command. If it does not exit cleanly, print the stderr
+    of the command to stderr"""
+    try:
+        subprocess.check_output(args, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print e.output
+        raise
+
+
 class StyleTest(unittest.TestCase):
     "run flake8 with the right arguments and ensure all files pass"
     def test_style(self):
-        try:
-            subprocess.check_output(['flake8', '--ignore=F', 'raco'],
-                                    stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            print >> sys.stderr, e.output
-            raise
+        "run flake8 with the right arguments and ensure all files pass"
+        check_output_and_print_stderr(['flake8', '--ignore=F', 'raco'])
+
+    def test_pylint(self):
+        "run pylint -E to catch obvious errors"
+        check_output_and_print_stderr(['pylint', '-E', 'raco'])
