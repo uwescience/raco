589b596029358681761d2028cb98236748fb6303
Andrew Whitaker <whitaker@cs.washington.edu>
2014-07-21 16:45:14 -0700

    Poll waiting for query completion

--- raco/run_federated.py
+++ raco/run_federated.py
@@ -3,6 +3,18 @@ from raco.language import FederatedAlgebra
 from raco.algebra import LogicalAlgebra, Sequence, ExportMyriaToScidb
 from raco.federatedlang import *
 
+import time
+
+def wait_for_completion(conn, query_id, repeats=10):
+    for i in range(repeats):
+        res = conn.get_query_status(query_id)
+        status = res['status']
+        print status
+        if status == 'SUCCESS':
+            return res
+        time.sleep(1)
+
+    raise Exception("Query failure")
 
 def run(logical_plan, myria_conn, scidb_conn_factory):
     seq_op = optimize(logical_plan, target=FederatedAlgebra(),
@@ -15,9 +27,11 @@ def run(logical_plan, myria_conn, scidb_conn_factory):
             sdb = scidb_conn_factory.connect(op.connection)
             sdb._execute_query(op.command)
         elif isinstance(op, RunMyria):
-            outs.append(myria_conn.submit_query(op.command))
-            # Wait for completion...
+            res = myria_conn.submit_query(op.command)
+            _id = res['queryId']
+            outs.append(wait_for_completion(myria_conn, _id))
         elif isinstance(op, ExportMyriaToScidb):
+            pass
             # Fetch result schema
             # Fetch result
             # Store as scidb array
