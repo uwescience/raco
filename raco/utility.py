ea2539a4429639d534e904668354f49d945d2e02
Daniel Halperin <dhalperi@cs.washington.edu>
2014-07-24 13:24:50 -0700

    utility: add str_list and str_list_inner helpers
    These convert lists to string using str, instead of repr, for the member objects. The
    _inner variant does not append the brackets, as this is widely used in operator shortStr function.

--- raco/utility.py
+++ raco/utility.py
@@ -8,6 +8,17 @@ def emitlist(argslist):
     return "\n".join([str(x) for x in argslist if len(str(x)) > 0])
 
 
+def str_list_inner(L):
+    """Convert members of L to a comma-separated string.
+    List brackets are not added."""
+    return ", ".join([str(obj) for obj in L])
+
+
+def str_list(L):
+    """Convert L to a string using str rather than repr for list members."""
+    return "[{inn}]".format(inn=str_list_inner(L))
+
+
 class Printable(object):
     @classmethod
     def opname(cls):
