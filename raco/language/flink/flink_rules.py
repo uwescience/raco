b1658c527267ac376fcc86b6ff233798947b4161
Daniel Halperin <dhalperi@cs.washington.edu>
2014-08-27 17:35:07 -0700

    flink.rules: add FlinkProjectingJoin

--- raco/language/flink/flink_rules.py
+++ raco/language/flink/flink_rules.py
@@ -1,6 +1,7 @@
 import raco.algebra as algebra
 from raco.expression import (AttributeRef, UnnamedAttributeRef,
-                             NumericLiteral, COUNTALL, COUNT, SUM, MIN, MAX)
+                             NumericLiteral, COUNTALL, COUNT, SUM, MIN, MAX,
+                             to_unnamed_recursive, reindex_expr)
 import raco.rules as rules
 
 
@@ -14,14 +15,16 @@ class FlinkAlgebra(object):
                      rules.MergeSelects(),
                      rules.ProjectingJoin(),
                      rules.JoinToProjectingJoin()]
-        flink_rules = [FlinkGroupBy()]
-        post_rules = [rules.PushApply(),
-                      rules.RemoveUnusedColumns(),
-                      rules.PushApply(),
-                      rules.RemoveUnusedColumns(),
-                      rules.PushApply(),
-                      rules.RemoveNoOpApply()]
-        return pre_rules + flink_rules + post_rules
+        cleanup_rules = [rules.PushApply(),
+                         rules.RemoveUnusedColumns(),
+                         rules.PushApply(),
+                         rules.RemoveUnusedColumns(),
+                         rules.PushApply(),
+                         rules.RemoveNoOpApply()]
+        flink_group_by = [FlinkGroupBy()]
+        flink_projecting_join = [FlinkProjectingJoin()]
+        return (pre_rules + flink_group_by + cleanup_rules
+                + flink_projecting_join + cleanup_rules)
 
 
 class FlinkGroupBy(rules.Rule):
@@ -57,4 +60,48 @@ class FlinkGroupBy(rules.Rule):
         apply = algebra.Apply(emitters=[(None, e) for e in emit_exprs],
                               input=op.input)
         op.input = apply
+        return op
+
+
+class FlinkProjectingJoin(rules.Rule):
+    """Flink cannot emit columns in a different order than they come in.
+    Fix this by permuting them on the way in."""
+
+    def fire(self, op):
+        if not isinstance(op, algebra.ProjectingJoin):
+            return op
+
+        left_scheme = op.left.scheme()
+        right_scheme = op.right.scheme()
+        left_len = len(left_scheme)
+        input_scheme = left_scheme + right_scheme
+        output_cols = [c.get_position(input_scheme) for c in op.output_columns]
+        sorted_cols = sorted(output_cols)
+        if sorted_cols == output_cols:
+            return op
+
+        # Swap the correct columns in the left child using an Apply
+        left_cols = [c for c in output_cols if c < left_len]
+        left_col_map = {c: s for c, s in zip(left_cols, sorted(left_cols))}
+        left_refs = [UnnamedAttributeRef(left_col_map.get(i) or i)
+                     for i in range(left_len)]
+        emitters = [(None, ref) for ref in left_refs]
+        left_apply = algebra.Apply(emitters=emitters, input=op.left)
+
+        # Swap the correct columns in the right child using an Apply
+        right_cols = [c - left_len for c in output_cols if c >= left_len]
+        right_col_map = {c: s for c, s in zip(right_cols, sorted(right_cols))}
+        right_refs = [UnnamedAttributeRef(right_col_map.get(i) or i)
+                      for i in range(len(right_scheme))]
+        emitters = [(None, ref) for ref in right_refs]
+        right_apply = algebra.Apply(emitters=emitters, input=op.left)
+
+        # Reindex the join condition and output columns
+        join_reindex_map = {c: s for c, s in zip(sorted_cols, output_cols)}
+        reindex_expr(op.condition, join_reindex_map)
+
+        op.output_columns = [UnnamedAttributeRef(s) for s in sorted_cols]
+        op.left = left_apply
+        op.right = right_apply
+
         return op
