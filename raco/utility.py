a18bbe4c2986ff6331605242cfeee26cb32a6dc6
Daniel Halperin <dhalperi@cs.washington.edu>
2014-09-03 11:15:12 -0700

    raco: fix string utility functions with containers
    We want to stringify operators in a way that recursively stringifies
    containers. I had previously hacked this up to work for non-nested
    lists, but of course, NaryJoin uses nested lists. Fix this by rewriting
    real_str to work with arbitrary containers.

--- raco/utility.py
+++ raco/utility.py
@@ -1,3 +1,6 @@
+import collections
+
+
 def emit(*args):
     """Return blocks of code as a string."""
     return "\n".join([str(x) for x in args if len(str(x)) > 0])
@@ -8,15 +11,33 @@ def emitlist(argslist):
     return "\n".join([str(x) for x in argslist if len(str(x)) > 0])
 
 
-def str_list_inner(L):
-    """Convert members of L to a comma-separated string.
-    List brackets are not added."""
-    return ", ".join([str(obj) for obj in L])
+def real_str(obj, skip_out=False):
+    """Convert the input object to a string, recursively stringifying elements
+    inside of containers. If skip_out is True, the container bounds will not
+    be displayed. E.g. real_str([1, 2]) == "[1,2]" but
+    real_str([1, 2], skip_out=True) == "1,2"."""
+    if isinstance(obj, collections.Container):
+        if isinstance(obj, collections.Sequence):
+            inner = ','.join(real_str(e) for e in obj)
+            if skip_out:
+                return inner
+            return '[{inn}]'.format(inn=inner)
+        elif isinstance(obj, collections.Mapping):
+            inner = ','.join('{a}: {b}'.format(a=real_str(a), b=real_str(b))
+                             for a, b in obj.elements())
+            if skip_out:
+                return inner
+            return '{{{inn}}}'.format(inn=inner)
+        elif isinstance(obj, collections.Set):
+            inner = ','.join(real_str(e) for e in obj)
+            if skip_out:
+                return inner
+            return '{{{inn}}}'.format(inn=inner)
+        else:
+            raise NotImplementedError(
+                "real_str(obj) for type(obj)={t}".format(t=type(obj)))
 
-
-def str_list(L):
-    """Convert L to a string using str rather than repr for list members."""
-    return "[{inn}]".format(inn=str_list_inner(L))
+    return str(obj)
 
 
 class Printable(object):
