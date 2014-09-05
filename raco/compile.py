1e2c7c93b6a72f3f9cc8a5e437d5d5cda549ed79
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-05 15:02:06 -0700

    create dots for all optimization steps for RACO_OPTIMIZER_GRAPHS

--- raco/compile.py
+++ raco/compile.py
@@ -2,9 +2,12 @@ from raco import algebra
 import raco.language as language
 from pipelines import Pipelined
 from raco.utility import emit
+import raco.viz as viz
+import os
 
 import logging
 LOG = logging.getLogger(__name__)
+
 
 """
 Apply rules to an expression
@@ -14,14 +17,38 @@ target algebra
 """
 
 
+class PlanWriter():
+
+    def __init__(self, template="wip-%02d.physical.dot", limit=20):
+        self.ind = 0
+        self.template = template
+        self.limit = limit
+        self.enabled = os.environ.get('RACO_OPTIMIZER_GRAPHS') in \
+            ['true', 'True', 't', 'T', '1', 'yes', 'y']
+
+    def write_if_enabled(self, plan, title):
+        if self.enabled:
+            with open(self.template % self.ind, 'w') as dwf:
+                dwf.write(viz.operator_to_dot(plan, title=title))
+
+        self.ind += 1
+
+
 def optimize_by_rules(expr, rules):
+    writer = PlanWriter()
+    writer.write_if_enabled(expr, "before rules")
+
     for rule in rules:
-        def recursiverule(expr):
-            newexpr = rule(expr)
+        def recursiverule(e):
+            newe = rule(e)
+            writer.write_if_enabled(newe, str(rule))
+
             LOG.debug("apply rule %s\n--- %s => %s", rule, expr, newexpr)
-            newexpr.apply(recursiverule)
-            return newexpr
+            newe.apply(recursiverule)
+
+            return newe
         expr = recursiverule(expr)
+
     return expr
 
 
