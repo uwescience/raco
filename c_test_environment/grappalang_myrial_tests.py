30fc619af01c233273b30a7cf93c406048e83040
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-09 21:10:24 -0700

    test symmetric hash join

--- c_test_environment/grappalang_myrial_tests.py
+++ c_test_environment/grappalang_myrial_tests.py
@@ -4,6 +4,7 @@ from testquery import GrappalangRunner
 from generate_test_relations import generate_default
 from generate_test_relations import need_generate
 from raco.language.grappalang import GrappaAlgebra
+import raco.language.grappalang as grappalang
 from platform_tests import MyriaLPlatformTestHarness, MyriaLPlatformTests
 from raco.compile import compile
 from nose.plugins.skip import SkipTest
@@ -34,7 +35,11 @@ class MyriaLGrappaTest(MyriaLPlatformTestHarness, MyriaLPlatformTests):
     def check(self, query, name, **kwargs):
         gname = "grappa_{name}".format(name=name)
 
+        if kwargs.get('join_type', None) == 'symmetric_hash':
+            kwargs['join_type'] = grappalang.GrappaSymmetricHashJoin
+
         kwargs['target_alg'] = GrappaAlgebra()
+
         plan = self.get_physical_plan(query, **kwargs)
         physical_dot = viz.operator_to_dot(plan)
 
