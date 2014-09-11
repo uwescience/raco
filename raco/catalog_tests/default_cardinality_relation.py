7c32214c0394e96fe18b4ab18c2c9d20d7a47de6
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-11 14:56:29 -0700

    tests for cardinality and no cardinality

---
+++ raco/catalog_tests/default_cardinality_relation.py
@@ -0,0 +1,3 @@
+{'A': [('a', 'DOUBLE_TYPE'), ('b', 'STRING_TYPE')],
+ 'B': [('x', 'DOUBLE_TYPE'), ('y', 'STRING_TYPE'), ('z', 'LONG_TYPE')]
+ }
---
+++ raco/catalog_tests/test_catalog.py
@@ -0,0 +1,31 @@
+import unittest
+
+from raco.catalog import FromFileCatalog
+from raco.catalog import DEFAULT_CARDINALITY
+
+test_file_path = "raco/catalog_tests"
+
+
+class TestFromFileCatalog(unittest.TestCase):
+    def test_default_cardinality_relation(self):
+        cut = FromFileCatalog.load_from_file(
+            "{p}/default_cardinality_relation.py".format(p=test_file_path))
+
+        self.assertEqual(cut.get_scheme('B').get_names(),
+                         ['x', 'y', 'z'])
+        self.assertEqual(cut.get_scheme('A').get_types(),
+                         ['DOUBLE_TYPE', 'STRING_TYPE'])
+
+        self.assertEqual(cut.num_tuples('A'), DEFAULT_CARDINALITY)
+        self.assertEqual(cut.num_tuples('B'), DEFAULT_CARDINALITY)
+
+    def test_set_cardinality_relation(self):
+        cut = FromFileCatalog.load_from_file(
+            "{p}/set_cardinality_relation.py".format(p=test_file_path))
+
+        self.assertEqual(cut.get_scheme('C').get_names(),
+                         ['a', 'b', 'c'])
+        self.assertEqual(cut.num_tuples('B'), DEFAULT_CARDINALITY)
+        self.assertEqual(cut.num_tuples('C'), 12)
+
+
