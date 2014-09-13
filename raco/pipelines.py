f70c012e3c23d788c411f7bcfb6423fd62afcaf2
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-12 19:12:01 -0700

    flake8 for pipelines

--- raco/pipelines.py
+++ raco/pipelines.py
@@ -1,5 +1,3 @@
-# TODO: make it pass with flake8 test
-# flake8: noqa
 
 import abc
 from raco.utility import emitlist
@@ -7,6 +5,7 @@ from algebra import gensym
 
 import logging
 LOG = logging.getLogger(__name__)
+
 
 class ResolvingSymbol:
     def __init__(self, name):
@@ -18,6 +17,7 @@ class ResolvingSymbol:
 
     def getName(self):
         return self._name
+
 
 class CompileState:
 
@@ -69,7 +69,8 @@ class CompileState:
         """
         Like getPipelineProperty but returns None if no property is found
         """
-        LOG.debug("get(to check) %s from %s" % (key, self.current_pipeline_properties))
+        LOG.debug("get(to check) %s from %s"
+                  % (key, self.current_pipeline_properties))
         return self.current_pipeline_properties.get(key)
 
     def createUnresolvedSymbol(self):
@@ -100,9 +101,11 @@ class CompileState:
 
     def addPipeline(self, p):
         LOG.debug("output pipeline %s", self.current_pipeline_properties)
-        pipeline_code = emitlist(self.current_pipeline_precode) +\
-                        self.language.pipeline_wrap(self.pipeline_count, p, self.current_pipeline_properties) +\
-                        emitlist(self.current_pipeline_postcode)
+        pipeline_code = \
+            emitlist(self.current_pipeline_precode) +\
+            self.language.pipeline_wrap(self.pipeline_count, p,
+                                        self.current_pipeline_properties) +\
+            emitlist(self.current_pipeline_postcode)
 
         # force scan pipelines to go first
         if self.current_pipeline_properties.get('type') == 'scan':
@@ -131,19 +134,22 @@ class CompileState:
         self.flush_pipelines.append(c)
 
     def getInitCode(self):
-        # inits is a set
+
+        # inits is a set.
         # If this ever becomes a bottleneck when declarations are strings,
         # as in clang, then resort to at least symbol name deduping.
-        #TODO: better would be to mark elements of self.initializers as
-        #TODO: "do dedup" or "don't dedup"
+        # TODO: better would be to mark elements of self.initializers as
+        # TODO: "do dedup" or "don't dedup"
         s = set()
+
         def f(x):
-            if x in s: return False
+            if x in s:
+                return False
             else:
                 s.add(x)
                 return True
 
-        code = emitlist(filter(f,self.initializers))
+        code = emitlist(filter(f, self.initializers))
         return code % self.resolving_symbols
 
     def getDeclCode(self):
@@ -151,8 +157,10 @@ class CompileState:
         # If this ever becomes a bottleneck when declarations are strings,
         # as in clang, then resort to at least symbol name deduping.
         s = set()
+
         def f(x):
-            if x in s: return False
+            if x in s:
+                return False
             else:
                 s.add(x)
                 return True
@@ -165,12 +173,18 @@ class CompileState:
     def getExecutionCode(self):
         # list -> string
         scan_linearized = emitlist(self.scan_pipelines)
-        mem_linearized = emitlist(self.pipelines) + emitlist(self.main_wait_statements)
+        mem_linearized = \
+            emitlist(self.pipelines) + emitlist(self.main_wait_statements)
         flush_linearized = emitlist(self.flush_pipelines)
-        scan_linearized_wrapped = self.language.group_wrap(gensym(), scan_linearized, {'type': 'scan'})
-        mem_linearized_wrapped = self.language.group_wrap(gensym(), mem_linearized, {'type': 'in_memory'})
+        scan_linearize_wrap = self.language.group_wrap(gensym(),
+                                                       scan_linearized,
+                                                       {'type': 'scan'})
+        mem_linearize_wrap = self.language.group_wrap(gensym(),
+                                                      mem_linearized,
+                                                      {'type': 'in_memory'})
 
-        linearized = scan_linearized_wrapped + mem_linearized_wrapped + flush_linearized
+        linearized = \
+            scan_linearize_wrap + mem_linearize_wrap + flush_linearized
 
         # substitute all lazily resolved symbols
         resolved = linearized % self.resolving_symbols
@@ -184,7 +198,8 @@ class CompileState:
             LOG.debug("lookup subexpression %s -> %s", expr, res)
             return res
         else:
-            # if CSE is turned off then always return None for expression matches
+            # if CSE is turned off
+            # then always return None for expression matches
             return None
 
     def saveExpr(self, expr, sym):
@@ -210,34 +225,34 @@ class Pipelined(object):
     __metaclass__ = abc.ABCMeta
 
     def __markAllParents__(self):
-      root = self
+        root = self
 
-      def markChildParent(op):
-        for c in op.children():
-          c.parent = op
-        return []
+        def markChildParent(op):
+            for c in op.children():
+                c.parent = op
+            return []
 
-      [_ for _ in root.postorder(markChildParent)]
+        [_ for _ in root.postorder(markChildParent)]
 
     @abc.abstractmethod
     def produce(self, state):
-      """Denotation for producing a tuple"""
-      return
+        """Denotation for producing a tuple"""
+        return
 
     @abc.abstractmethod
     def consume(self, inputTuple, fromOp, state):
-      """Denotation for consuming a tuple"""
-      return
+        """Denotation for consuming a tuple"""
+        return
 
     def compilePipeline(self):
-      self.__markAllParents__()
+        self.__markAllParents__()
 
-      state = CompileState(self.language)
+        state = CompileState(self.language)
 
-      state.addCode( self.language.comment("Compiled subplan for %s" % self) )
+        state.addCode(self.language.comment("Compiled subplan for %s" % self))
 
-      self.produce(state)
+        self.produce(state)
 
-      #state.addCode( self.language.log("Evaluating subplan %s" % self) )
+        # state.addCode( self.language.log("Evaluating subplan %s" % self) )
 
-      return state
+        return state
