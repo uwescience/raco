5146cb3e24758388f92b6239611a2a4c6cab3f9d
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-19 19:04:14 -0700

    classifier in myrial

---
+++ examples/naivebayes/catalog.py
@@ -0,0 +1,7 @@
+{'public:adhoc:conditionals': [('index', 'LONG_TYPE'), ('lp', 'DOUBLE_TYPE'), ('outcome', 'LONG_TYPE'), ('value', 'INT_TYPE')],
+'public:adhoc:testdata': [('id', 'LONG_TYPE'), ('x0', 'DOUBLE_TYPE'),
+('x1', 'DOUBLE_TYPE'),
+('x2', 'DOUBLE_TYPE'),
+('x3', 'DOUBLE_TYPE'),
+('x4', 'DOUBLE_TYPE')]
+}
