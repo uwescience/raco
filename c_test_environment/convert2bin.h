4fbf486e139fa2ebedfa54a2d09b1f729d80814d
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-21 10:11:52 -0700

    converter utility

---
+++ c_test_environment/convert2bin.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <sstream>
+#include <fstream>
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+std::ostream& CHECK(bool cond) {
+  if (!cond) {
+    std::cerr << "assertion failed" << std::endl;
+    exit(1);
+  }
+}
+
+
+template< typename Tuple >
+void convert2bin_withTuple( std::string fn, uint64_t burn=0 ) {
+  std::ifstream infile(fn, std::ifstream::in);
+  CHECK( infile.is_open() );// << fn << " failed to open";
+  
+  std::string outpath = fn+".bin";
+  std::ofstream outfile(outpath, std::ios_base::out | std::ios_base::binary );
+  CHECK( outfile.is_open() );// << outpath << " failed to open";
+  
+  int64_t linenum = 0;
+  while( infile.good() ) {
+    std::string line;
+    std::getline( infile, line );
+    if (line.length() == 0) break; // takes care of EOF
+
+    std::istringstream iss(line);
+    auto t = Tuple::fromIStream(iss);
+    
+    outfile.write((char*) &(t._fields), sizeof(Tuple)); 
+    linenum++;
+  }
+  infile.close();
+  outfile.close();
+  std::cout << "binary: " << outpath << std::endl;
+  std::cout << "rows: " << linenum << std::endl;
+  std::cout << "cols: " << Tuple::numFields() << std::endl;
+}
