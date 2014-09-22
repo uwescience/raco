ac376c2edbe5e348e84817748fb0957ed6d2effc
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-21 17:07:28 -0700

    LONG TYPE

--- examples/naivebayes/catalog.py
+++ examples/naivebayes/catalog.py
@@ -1,4 +1,4 @@
-{'public:adhoc:conditionals': [('index', 'LONG_TYPE'), ('lp', 'DOUBLE_TYPE'), ('outcome', 'LONG_TYPE'), ('value', 'INT_TYPE')],
+{'public:adhoc:conditionals': [('index', 'LONG_TYPE'), ('lp', 'DOUBLE_TYPE'), ('outcome', 'LONG_TYPE'), ('value', 'LONG_TYPE')],
 'public:adhoc:testdata': [('id', 'LONG_TYPE'), ('x0', 'DOUBLE_TYPE'),
 ('x1', 'DOUBLE_TYPE'),
 ('x2', 'DOUBLE_TYPE'),
