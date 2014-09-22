413c97f4d45010e232a68f88e88bf860f7bec111
Brandon Myers <bdmyers@cs.washington.edu>
2014-09-21 23:13:14 -0700

    fix concat schemes

--- examples/naivebayes/cat_scheme.py
+++
@@ -1,7 +0,0 @@
-
-from raco.catalog import FromFileCatalog
-import sys
-
-c1 = FromFileCatalog.load_from_file(sys.arv[1])
-c2 = FromFileCatalog.load_from_file(sys.arv[2])
-FromFileCatalog.print_cat(c1, c2)
