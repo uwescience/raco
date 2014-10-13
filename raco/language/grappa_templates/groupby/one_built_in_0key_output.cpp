6b36d09566e86286343de19ffccf0ec4feac8c73
Brandon Myers <bdmyers@cs.washington.edu>
2014-10-13 16:34:47 -0700

    remove newlines in template

--- raco/language/grappa_templates/groupby/one_built_in_0key_output.cpp
+++ raco/language/grappa_templates/groupby/one_built_in_0key_output.cpp
@@ -1,8 +1,6 @@
 {% extends '0key_output.cpp' %}
 
-{% block templateargs %}
-{{state_type}}, counter, &{{update_func}}, &get_count
-{% endblock %}
+{% block templateargs %}{{state_type}}, counter, &{{update_func}}, &get_count{% endblock %}
 
 {% block output %}
 {{output_tuple_type}} {{output_tuple_name}};
