ccb031825e8abb4e87dd65afcfae558ca67ff50e
Brandon Myers <bdmyers@cs.washington.edu>
2014-10-23 18:33:09 -0700

    fix syntax error in sql of multi key joins

--- c_test_environment/testqueries/two_key_hash_join.sql
+++ c_test_environment/testqueries/two_key_hash_join.sql
@@ -1,1 +1,1 @@
-select R3.c, T3.c where R3.a=T3.b and R3.b=T3.a;
+select R3.c, T3.c from R3, T3 where R3.a=T3.b and R3.b=T3.a;
--- c_test_environment/testqueries/two_key_hash_join_swap.sql
+++ c_test_environment/testqueries/two_key_hash_join_swap.sql
@@ -1,1 +1,1 @@
-select R3.c, T3.c where R3.a=T3.a and R3.b=T3.b;
+select R3.c, T3.c from R3, T3 where R3.a=T3.a and R3.b=T3.b;
