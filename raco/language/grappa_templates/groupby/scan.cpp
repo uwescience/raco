dd591838f25f18ade1044ed8812cd91dfcd97241
Brandon Myers <bdmyers@cs.washington.edu>
2014-10-23 18:12:12 -0700

    forgot to add scan.pp changes

--- raco/language/grappa_templates/groupby/scan.cpp
+++ raco/language/grappa_templates/groupby/scan.cpp
@@ -1,5 +1,5 @@
 {{hashname}}->forall_entries<&{{pipeline_sync}}>([=](std::pair<const {{keytype}},{{emit_type}}>&{{mapping_var_name}}) {
-    {{output_tuple_type}} {{output_tuple_name}}({% block initializer %}{% endblock %});
+    {{output_tuple_type}} {{output_tuple_name}}({% block initializer %}{{mapping_var_name}}.first{% endblock %});
     {{inner_code}}
 });
 
