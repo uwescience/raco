df114f811816eec2109f6b0f121d86ced32749a4
Brandon Myers <bdmyers@cs.washington.edu>
2014-10-31 11:56:42 -0700

    flake

--- raco/language/clangcommon.py
+++ raco/language/clangcommon.py
@@ -48,7 +48,8 @@ class CBaseLanguage(Language):
         # StrictUndefined makes uses of the result of render() fail when
         # a template variable is undefined, which is most useful for debugging
         return jinja2.Environment(
-            undefined=jinja2.StrictUndefined, loader=jinja2.ChoiceLoader(loaders))
+            undefined=jinja2.StrictUndefined,
+            loader=jinja2.ChoiceLoader(loaders))
 
     @classmethod
     @abc.abstractmethod
