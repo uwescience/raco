4a651bef6d07867d19d88d33659c37beb8ff2751
Brandon Myers <bdmyers@cs.washington.edu>
2014-11-04 22:51:07 -0800

    add std:: in previous commit

--- raco/language/grappa_templates/groupby/one_built_in_scan.cpp
+++ raco/language/grappa_templates/groupby/one_built_in_scan.cpp
@@ -1,3 +1,3 @@
 {% extends 'scan.cpp' %}
 
-{% block initializer %}std::tuple_cat({{ super() }}, make_tuple({{mapping_var_name}}.second)){% endblock %}
+{% block initializer %}std::tuple_cat({{ super() }}, std::make_tuple({{mapping_var_name}}.second)){% endblock %}
