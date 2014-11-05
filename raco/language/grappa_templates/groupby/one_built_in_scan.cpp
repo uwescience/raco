c65197b109d5a23ffea1334a99cc376f2bde62b5
Brandon Myers <bdmyers@cs.washington.edu>
2014-11-04 22:49:43 -0800

    has introduced another error in groupby scan for single-value agg

--- raco/language/grappa_templates/groupby/one_built_in_scan.cpp
+++ raco/language/grappa_templates/groupby/one_built_in_scan.cpp
@@ -1,3 +1,3 @@
 {% extends 'scan.cpp' %}
 
-{% block initializer %}std::tuple_cat({{ super() }}, {{mapping_var_name}}.second){% endblock %}
+{% block initializer %}std::tuple_cat({{ super() }}, make_tuple({{mapping_var_name}}.second)){% endblock %}
