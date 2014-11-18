82861a098e836e26b513622332610e23d7e9806a
TheYorkWei <york.wei@gmail.com>
2014-11-18 15:30:26 -0800

    small fix

--- raco/language/grappalang.py
+++ raco/language/grappalang.py
@@ -999,7 +999,7 @@ class GrappaStore(clangcommon.BaseCStore, GrappaOperator):
 
         filename = (str(self.relation_key).split(":")[2])
         outputnamedecl = define_cl_arg(
-            'string', '{0}.bin'.format(filename), "Output File")
+            'string', '{0}.bin'.format(filename), "", "Output File")
 
         state.addDeclarations([outputnamedecl])
         names = [x.encode('UTF8') for x in my_sch.get_names()]
