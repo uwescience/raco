7db208cdbfc97ef5c502d6262d2938badb640311
Brandon Myers <bdmyers@cs.washington.edu>
2015-05-13 11:00:06 -0700

    need to change how set func is used

--- raco/language/grappa_templates/groupby/one_built_in_0key_output.cpp
+++ raco/language/grappa_templates/groupby/one_built_in_0key_output.cpp
@@ -4,6 +4,6 @@
 
 {% block output %}
 {{output_tuple_type}} {{output_tuple_name}};
-{{output_tuple_set_func}}({{output_tuple_name}}_tmp);
+{{output_tuple_set_func}} = {{output_tuple_name}}_tmp;
 {% endblock %}
 
