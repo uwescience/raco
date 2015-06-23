7701d870fb1dedb2f68bb1a661d360d30d8b9418
Brandon Myers <bdmyers@cs.washington.edu>
2015-06-23 15:01:22 -0700

    typo with quotes

--- raco/language/grappa_templates/relation_declaration.cpp
+++ raco/language/grappa_templates/relation_declaration.cpp
@@ -1,3 +1,3 @@
 DEFINE_string(input_file_{{name}}, "{{name}}", "Input file");
 Relation<{{tuple_type}}> {{resultsym}};
-std::vector<std::string> schema_{{resultsym}} = { {% for c in colnames %}"{{c}},"{% endfor %} };
+std::vector<std::string> schema_{{resultsym}} = { {% for c in colnames %}"{{c}}",{% endfor %} };
