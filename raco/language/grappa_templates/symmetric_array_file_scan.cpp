84bdf845ae965ad50f58158a76fe79d0b0e58b8e
Brandon Myers <bdmyers@cs.washington.edu>
2015-06-29 15:01:08 -0700

    forgot one from previous PR; so much for test coverage :)

---
+++ raco/language/grappa_templates/symmetric_array_file_scan.cpp
@@ -0,0 +1,10 @@
+if (FLAGS_bin) {
+BinaryRelationFileReader<{{result_type}},
+                           aligned_vector<{{result_type}}>,
+                           SymmetricArrayRepresentation<{{result_type}}>> reader;
+                           {{resultsym}} = reader.read( FLAGS_input_file_{{name}} + ".bin" );
+                           } else {
+
+                           CHECK(false) << "only --bin=true supported for symmetric array repr"
+
+                           }
