facf2bf29784f4e2f3b4eaad62e324d3c24e6492
Brandon Myers <bdmyers@cs.washington.edu>
2015-06-29 15:06:31 -0700

    and here is your test coverage

--- raco/platform_tests.py
+++ raco/platform_tests.py
@@ -228,11 +228,8 @@ class MyriaLPlatformTests(object):
         """, "scan")
 
     def test_select(self):
-        self.check_sub_tables("""
-        T1 = SCAN(%(T1)s);
-        x = [FROM T1 WHERE a>5 EMIT a];
-        STORE(x, OUTPUT);
-        """, "select")
+        q = self.myrial_from_sql(['T1'], "select")
+        self.check(q, "select")
 
     def test_join(self):
         self.check_sub_tables("""
