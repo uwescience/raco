6dc0a10a5b531e78fd133b116414b3c990a55020
Brandon Myers <bdmyers@cs.washington.edu>
2015-07-08 16:55:02 -0700

    range is exclusive?

--- raco/language/cbase_templates/materialized_tuple_ref.cpp
+++ raco/language/cbase_templates/materialized_tuple_ref.cpp
@@ -28,7 +28,7 @@
     static void print_representation() {
         const {{tupletypename}} _t;
 
-        {% for i in range(1, numfields-1) %}
+        {% for i in range(1, numfields) %}
         std::cout << (((char*)&_t.f{{i}}) - ((char*)&_t.f{{i-1}})) << ",";
         {% endfor %}
         std::cout << (_t.fieldsSize() - (((char*)&_t.f{{numfields-2}}) - ((char*)&_t)));
