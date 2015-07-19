3fc1767e5e0bd5eec739a59f3ffed9afbdb30048
Bill Howe <billhowe@cs.washington.edu>
2015-07-19 16:02:32 -0700

    First pass at a federated catalog object

---
+++ raco/backends/federated/catalog.py
@@ -0,0 +1,35 @@
+from raco.catalog import Catalog
+import raco.scheme as scheme
+
+class FederatedCatalog(Catalog):
+
+    def __init__(self, catalogs=[]):
+        self.catalogs = catalogs
+
+    def _return_first(self, method, rel_key):
+        sch = None
+        for cat in self.catalogs:
+            try:
+                response = getattr(cat, method)(rel_key)
+                return (cat, response)
+            except:
+                # TODO: Catalogs should throw LookupError or KeyError; this is dangerous
+                # Assumes any error is a LookupError
+                continue
+
+        if not sch:
+            raise LookupError("Relation {} not found".format(rel_key))
+  
+    def sourceof(self, rel_key):
+        cat, sch = self._return_first("get_scheme", rel_key)
+        return cat
+
+    def get_num_servers(self):
+        raise NotImplemented
+
+    def get_scheme(self, rel_key):
+        cat, sch = self._return_first("get_scheme", rel_key)
+        return sch
+
+    def num_tuples(self, rel_key):
+        return self._return_first("num_tuples", rel_key)
