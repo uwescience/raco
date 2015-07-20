96ad563b92e893b79d10597fc047e155a0c34ea9
Bill Howe <billhowe@cs.washington.edu>
2015-07-19 22:40:43 -0700

    Some name changes to better reflect the design

--- raco/backends/federated/algebra.py
+++ raco/backends/federated/algebra.py
@@ -4,8 +4,8 @@ from raco.backends import Language, Algebra
 from raco.backends.myria import MyriaLeftDeepTreeAlgebra as MyriaAlgebra
 from raco.compile import optimize
 from raco.language.myrialang import compile_to_json
+import raco.algebra
 
-from raco.algebra import gensym
 
 class Federated(Language):
     pass
@@ -26,10 +26,10 @@ class FederatedOperator(algebra.ZeroaryOperator):
     def scheme(self):
         raise NotImplementedError()
 
-class Exec(FederatedOperator):
+class LogicalExec(FederatedOperator):
     pass
 
-class Mover(FederatedOperator):
+class LogicalMove(FederatedOperator):
     def __init__(self, sourcename, sourcecatalog, targetname, targetcatalog):
         self.sourcename = sourcename
         self.sourcecatalog = sourcecatalog
@@ -47,31 +47,28 @@ class Mover(FederatedOperator):
 
 #class RunMyria(Runner):
 #    """Run a Myria query on the UW cluster"""
-#
+
 #    def __repr__(self):
 #        return "RunMyria(%s, %s)" % (self.command, self.connection)
-#
+
 #    def num_tuples(self):
 #      raise NotImplementedError("{op}.num_tuples".format(op=type(self)))
 
-class ImportSciDBToMyria(Mover):
-    pass
-
 #class ExportMyriaToScidb(Mover):
 #    def __init__(self, myria_relkey, scidb_array_name, conn):
-#        self.scidb_array_name = scidb_array_name
+#       self.scidb_array_name = scidb_array_name
 #        self.myria_relkey = myria_relkey
 #        self.connection = conn
 
 #    def shortStr(self):
 #        return "ExportToScidb(%s, %s)" % (self.myria_relkey,
-                                          self.scidb_array_name)
+                                          #self.scidb_array_name)
 
 #    def copy(self, other):
 #        self.scidb_array_name = other.scidb_array_name
 #        self.myria_relkey = other.myria_relkey
 #        self.connection = other.connection
-
+#
 #    def scheme(self):
 #        raise NotImplementedError()
 
@@ -83,12 +80,12 @@ class Dispatch(rules.Rule):
     def fire(self, expr):
         if isinstance(expr, algebra.Sequence):
             return expr  # Retain top-level sequence operator
-        if isinstance(expr, algebra.ImportSciDBToMyria):
+        if isinstance(expr, algebra.Move):
             return expr
         if isinstance(expr, algebra.ExecScan):
             # Some kind of custom code that we must pass through
             return dispatchmap[expr.languagetag](expr.command, expr.connection)
-        if isinstance(expr, Exec):
+        if isinstance(expr, LogicalExec):
             return dispatchmap[expr.languagetag](expr.command, expr.connection)
         else:
             # Just a logical plan that we will dispatch to Myria by default
@@ -118,17 +115,18 @@ Maybe rule traversal is not bottom-up?"
     def __init__(self, catalog):
         # Assumes this is a Federated Catalog
         self.federatedcatalog = catalog
+        super(self.__class__, self).__init__()
 
     @classmethod
     def checkchild(cls, child):
-        if not isinstance(child, Exec):
+        if not isinstance(child, LogicalExec):
             raise ValueError(err.format(child))
         
     def fire(self, op):
         if isinstance(op, raco.algebra.Scan):
             # TODO: Assumes each relation is in only one catalog
             cat = self.federatedcatalog.sourceof(op.relation_key)
-            return Exec(op, cat)
+            return LogicalExec(op, cat)
 
         if isinstance(op, raco.algebra.UnaryOperator):
            self.checkchild(op.child)
@@ -149,7 +147,7 @@ Maybe rule traversal is not bottom-up?"
            if leftcatalog == rightcatalog:
                op.left = op.left.plan
                op.right = op.right.plan
-               newexec = Exec(op, leftcatalog)
+               newexec = LogicalExec(op, leftcatalog)
                return newexec
 
            else: 
@@ -158,7 +156,7 @@ Maybe rule traversal is not bottom-up?"
                    # We need to move a dataset
 
                    # Give it a name
-                   movedrelation = gensym()
+                   movedrelation = raco.algebra.gensym()
 
                    # Add a store operation on the SciDB side
                    scidbwork = op.right
@@ -189,27 +187,33 @@ Maybe rule traversal is not bottom-up?"
                    temp = op.left
                    op.left = op.right
                    op.right = temp
-                   return self.fire(op)
+                   return self(op)
 
                else:
                    template = "Expected Myria or SciDB catalogs, got {}, {}"
                    msg = template.format(leftcatalog, rightcatalog)
                    raise NotImplemented(msg)
 
+        if isinstance(op, raco.algebra.Sequence):
+
         if isinstance(op, raco.algebra.NaryOperator):
             template = "NaryOperators not yet implemented, got {}"
             msg = template.format(op)
-            raise NotImplemented(msg)
+            raise NotImplementedError(msg)
 
 class FederatedAlgebra(Algebra):
     language = Federated
 
-    operators = [Exec, ImportSciDBToMyria]
+    operators = [LogicalExec, LogicalMove]
 
-    def __init__(self, algebras):
+    def __init__(self, algebras, catalog):
+        '''
+        A list of algebras and a federated catalog
+        '''
         self.algebras = algebras
+        self.federatedcatalog = catalog
 
     def opt_rules(self, **kwargs):
         childrules = sum([a.opt_rules() for a in self.algebras],[])
-        fedrules = [SplitSciDBToMyria]
+        fedrules = [SplitSciDBToMyria(self.federatedcatalog)]
         return fedrules
