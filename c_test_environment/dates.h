194c4ec7bfd5d063b6232f7974479987ac20ffe0
Brandon Myers <bdmyers@cs.washington.edu>
2015-07-24 14:42:52 -0700

    conversion for array to string dates methods

--- c_test_environment/dates.h
+++ c_test_environment/dates.h
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <string>
+#include <array>
 
 // date format is a string YYYY-MM-DD
 //                         0123456789
@@ -10,6 +11,21 @@ namespace dates {
   uint64_t year(std::string date); 
   uint64_t month(std::string date);
   uint64_t day(std::string date);
+
+// for passing an array
+// TODO implicit conversion to avoid this code, https://github.com/uwescience/raco/issues/454
+  <template N>
+  uint64_t year(std::array<char, N> date) {
+    year(std::string(date.data()))
+  }
+  <template N>
+  uint64_t month(std::array<char, N> date) {
+    month(std::string(date.data()))
+  }
+  <template N>
+  uint64_t day(std::array<char, N> date) {
+    day(std::string(date.data()))
+  }
 
   std::string add(std::string t, int64_t days);
 }
