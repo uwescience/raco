1db334c78ce78cb429172658c8f85feb5fcef866
Brandon Myers <bdmyers@cs.washington.edu>
2015-08-17 19:28:16 -0700

    always broadcast relation name to everyone no matter what

--- raco/language/grappa_templates/symmetric_array_file_scan.cpp
+++ raco/language/grappa_templates/symmetric_array_file_scan.cpp
@@ -2,7 +2,13 @@ if (FLAGS_bin) {
 BinaryRelationFileReader<{{result_type}},
                            aligned_vector<{{result_type}}>,
                            SymmetricArrayRepresentation<{{result_type}}>> reader;
-                           {{resultsym}} = reader.read( FLAGS_input_file_{{name}} + ".bin" );
+                           // just always broadcast the name to all cores
+                           // although for some queries it is unnecessary
+                           auto l_{{resultsym}} = reader.read( FLAGS_input_file_{{name}} + ".bin" );
+                           on_all_cores([=] {
+                                {{resultsym}} = l_{{resultsym}};
+                           });
+
                            } else {
 
                            CHECK(false) << "only --bin=true supported for symmetric array repr";
