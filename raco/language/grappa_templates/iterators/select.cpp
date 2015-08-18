b347298ff234e6bc4b7abd6a0505225f92968db4
Brandon Myers <bdmyers@cs.washington.edu>
2015-08-17 19:08:07 -0700

    typo template

--- raco/language/grappa_templates/iterators/select.cpp
+++ raco/language/grappa_templates/iterators/select.cpp
@@ -2,6 +2,6 @@ class {{class_symbol}} : public Select<{{consume_type}}, {{produce_type}}> {
     using Select<{{produce_type}}, {{produce_type}}>::Select;
     protected:
         bool predicate({{produce_type}} {{consume_tuple_name}}) {
-            return {{expression}}
+            return {{expression}};
         }
 };
