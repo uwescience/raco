7ac86db5664f2974b78bb1e0f024a1fca14215e8
Brandon Myers <bdmyers@cs.washington.edu>
2015-08-17 19:15:41 -0700

    template typo

--- raco/language/grappa_templates/iterators/hashjoin_source.cpp
+++ raco/language/grappa_templates/iterators/hashjoin_source.cpp
@@ -1,13 +1,13 @@
 class {{class_symbol}} : public HashJoinSource<{{keytype}},
                                                 {{left_tuple_type}},
                                                 {{right_tuple_type}},
-                                                hash_tuple::hash<{{keytype}}>>
+                                                hash_tuple::hash<{{keytype}}>,
 {{out_tuple_type}}> {
 
     using HashJoinSource<{{keytype}},
                                                 {{left_tuple_type}},
                                                 {{right_tuple_type}},
-                                                hash_tuple::hash<{{keytype}}>>, {{out_tuple_type}}>::HashJoinSource;
+                                                hash_tuple::hash<{{keytype}}>, {{out_tuple_type}}>::HashJoinSource;
 
     protected:
         {{out_tuple_type}} mktuple({{left_tuple_type}}& {{left_name}}, {{right_tuple_type}}& {{right_name}}) {
