48e4e197619f15aa177e55de8f761c2aa80909fa
Brandon Myers <bdmyers@cs.washington.edu>
2015-08-28 11:41:30 -0700

    fix input naming bug turned up by q6

--- raco/language/grappa_templates/iterators/0key_groupby_source.cpp
+++ raco/language/grappa_templates/iterators/0key_groupby_source.cpp
@@ -1,7 +1,7 @@
 class {{class_symbol}} : public ZeroKeyAggregateSource<{{produce_type}}, {{state_type}}, {{combine_func}}> {
     using ZeroKeyAggregateSource<{{produce_type}}, {{state_type}}, {{combine_func}}>::ZeroKeyAggregateSource;
     protected:
-        void mktuple({{produce_type}}& dest, {{state_type}}& src) {
+        void mktuple({{produce_type}}& {{output_tuple}}, {{state_type}}& {{output_typle}}_tmp) {
             {{assignment_code}}
         }
 
