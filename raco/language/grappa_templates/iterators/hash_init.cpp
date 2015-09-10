641ea15f73a8d370f8c226520e701bfdc5c27b12
Brandon Myers <bdmyers@cs.washington.edu>
2015-09-10 14:26:41 -0700

    template typo

--- raco/language/grappa_templates/iterators/hash_init.cpp
+++ raco/language/grappa_templates/iterators/hash_init.cpp
@@ -1,2 +1,5 @@
-auto {{hashname}} = DHT_multi_symmetric_generic<{{keytype}}, {{left_type}}, {{right_type}}, hash_tuple::hash<{{keytype}}>::create_DHT_symmetric();
+auto {{hashname}} = DHT_multi_symmetric_generic<{{keytype}},
+                                                {{left_type}},
+                                                {{right_type}},
+                                                hash_tuple::hash<{{keytype}}>>::create_DHT_symmetric();
 
