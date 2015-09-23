d959d3f04b5f3a6195ded4ddb552bab0b7155d6f
Brandon Myers <bdmyers@cs.washington.edu>
2015-09-23 08:20:44 -0700

    fixes to groupby and shuffle

--- raco/language/grappa_templates/partition_groupby/nkey_update.cpp
+++ raco/language/grappa_templates/partition_groupby/nkey_update.cpp
@@ -1,2 +1,2 @@
 {{comment}}
-{{hashname}}->update_partition<{{input_type}}, &{{update_func}},&{{init_func}}>(std::make_tuple({{ keygets|join(',') }}), {{update_val}});
+{{hashname}}->template update_partition<{{input_type}}, &{{update_func}},&{{init_func}}>(std::make_tuple({{ keygets|join(',') }}), {{update_val}});
