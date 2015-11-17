e4c04ab4dd7b99b131b2994af2b47a952e7ab646
Brandon Myers <bdmyers@cs.washington.edu>
2015-11-16 17:02:33 -0800

    tests to catch such errors

--- raco/backends/myria/tests/test_myria_execution.py
+++ raco/backends/myria/tests/test_myria_execution.py
@@ -4,6 +4,8 @@ import json
 
 from raco.backends.myria.connection import MyriaConnection
 from raco.backends.myria.catalog import MyriaCatalog
+from raco.relation_key import RelationKey
+from raco.representation import RepresentationProperties
 
 import raco.myrial.interpreter as interpreter
 import raco.myrial.parser as myrialparser
@@ -30,6 +32,11 @@ def get_connection():
 
     return connection
 
+def rel_info(connection):
+    cat = MyriaCatalog(connection)
+    key = RelationKey('Brandon', 'Demo', 'MoreBooks')
+    return cat.num_tuples(key),\
+           cat.partitioning(key)
 
 def query(connection):
     # Get the physical plan for a test query
@@ -146,6 +153,17 @@ class TestQuery(unittest.TestCase):
         with HTTMock(local_mock):
             self.connection = get_connection()
         unittest.TestCase.__init__(self, args)
+
+    def test_num_tuples(self):
+        with HTTMock(local_mock):
+            i = rel_info(self.connection)
+            self.assertEqual(i[0], 50)
+
+    def test_partitioning(self):
+        with HTTMock(local_mock):
+            i = rel_info(self.connection)
+            self.assertEqual(i[1].hash_partitioned,
+                             RepresentationProperties().hash_partitioned)
 
     def test_submit(self):
         global query_request
