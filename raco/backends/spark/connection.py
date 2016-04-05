5a42cbaae08939c71c41a09bb44abe1ed0ab5b16
Shrainik Jain <shrainik@gmail.com>
2016-04-04 17:05:31 -0700

    creating a class and putting it in different file so that serialization doesn't fail.

--- raco/backends/spark/connection.py
+++ raco/backends/spark/connection.py
@@ -17,164 +17,9 @@ from pyspark import SparkConf
 from pyspark.sql.types import *
 from pyspark.sql import Row
 from pyspark.sql import SQLContext
+from pyspark_csv import PySpark_csv
 ### Adding helper methods csv taken from pyspark_csv.py
 #TODO: Fix later to figure out how to add a file
-
-import csv
-import dateutil.parser
-# from pyspark.sql.types import (StringType, DoubleType, TimestampType, NullType,
-#                                IntegerType, StructType, StructField)
-
-py_version = sys.version_info[0]
-
-def csvToDataFrame(sqlCtx, rdd, columns=None, sep=",", parseDate=True):
-    """Converts CSV plain text RDD into SparkSQL DataFrame (former SchemaRDD)
-    using PySpark. If columns not given, assumes first row is the header.
-    If separator not given, assumes comma separated
-    """
-    if py_version < 3:
-        def toRow(line):
-            return toRowSep(line.encode('utf-8'), sep)
-    else:
-        def toRow(line):
-            return toRowSep(line, sep)
-
-    rdd_array = rdd.map(toRow)
-    rdd_sql = rdd_array
-
-    if columns is None:
-        columns = rdd_array.first()
-        rdd_sql = rdd_array.zipWithIndex().filter(
-            lambda r_i: r_i[1] > 0).keys()
-    column_types = evaluateType(rdd_sql, parseDate)
-
-    def toSqlRow(row):
-        return toSqlRowWithType(row, column_types)
-
-    schema = makeSchema(zip(columns, column_types))
-
-    return sqlCtx.createDataFrame(rdd_sql.map(toSqlRow), schema=schema)
-
-
-def makeSchema(columns):
-    struct_field_map = {'string': StringType(),
-                        'date': TimestampType(),
-                        'double': DoubleType(),
-                        'int': IntegerType(),
-                        'none': NullType()}
-    fields = [StructField(k, struct_field_map[v], True) for k, v in columns]
-
-    return StructType(fields)
-
-
-def toRowSep(line, d):
-    """Parses one row using csv reader"""
-    for r in csv.reader([line], delimiter=d):
-        return r
-
-
-def toSqlRowWithType(row, col_types):
-    """Convert to sql.Row"""
-    d = row
-    for col, data in enumerate(row):
-        typed = col_types[col]
-        if isNone(data):
-            d[col] = None
-        elif typed == 'string':
-            d[col] = data
-        elif typed == 'int':
-            d[col] = int(round(float(data)))
-        elif typed == 'double':
-            d[col] = float(data)
-        elif typed == 'date':
-            d[col] = toDate(data)
-    return d
-
-
-# Type converter
-def isNone(d):
-    return (d is None or d == 'None' or
-            d == '?' or
-            d == '' or
-            d == 'NULL' or
-            d == 'null')
-
-
-def toDate(d):
-    return dateutil.parser.parse(d)
-
-
-def getRowType(row):
-    """Infers types for each row"""
-    d = row
-    for col, data in enumerate(row):
-        try:
-            if isNone(data):
-                d[col] = 'none'
-            else:
-                num = float(data)
-                if num.is_integer():
-                    d[col] = 'int'
-                else:
-                    d[col] = 'double'
-        except:
-            try:
-                toDate(data)
-                d[col] = 'date'
-            except:
-                d[col] = 'string'
-    return d
-
-
-def getRowTypeNoDate(row):
-    """Infers types for each row"""
-    d = row
-    for col, data in enumerate(row):
-        try:
-            if isNone(data):
-                d[col] = 'none'
-            else:
-                num = float(data)
-                if num.is_integer():
-                    d[col] = 'int'
-                else:
-                    d[col] = 'double'
-        except:
-            d[col] = 'string'
-    return d
-
-
-def reduceTypes(a, b):
-    """Reduces column types among rows to find common denominator"""
-    type_order = {'string': 0, 'date': 1, 'double': 2, 'int': 3, 'none': 4}
-    reduce_map = {'int': {0: 'string', 1: 'string', 2: 'double'},
-                  'double': {0: 'string', 1: 'string'},
-                  'date': {0: 'string'}}
-    d = a
-    for col, a_type in enumerate(a):
-        # a_type = a[col]
-        b_type = b[col]
-        if a_type == 'none':
-            d[col] = b_type
-        elif b_type == 'none':
-            d[col] = a_type
-        else:
-            order_a = type_order[a_type]
-            order_b = type_order[b_type]
-            if order_a == order_b:
-                d[col] = a_type
-            elif order_a > order_b:
-                d[col] = reduce_map[a_type][order_b]
-            elif order_a < order_b:
-                d[col] = reduce_map[b_type][order_a]
-    return d
-
-
-def evaluateType(rdd_sql, parseDate):
-    if parseDate:
-        return rdd_sql.map(getRowType).reduce(reduceTypes)
-    else:
-        return rdd_sql.map(getRowTypeNoDate).reduce(reduceTypes)
 
 class SparkConnection(object):
     """A Spark connection wrapper"""
@@ -191,12 +36,13 @@ class SparkConnection(object):
         # self.sparkcsv_python_file = "hdfs://" + self.masterhostname + ":9000/pyspark_csv.py"
         # self.context.addPyFile(self.sparkcsv_python_file)
         # print 'DEBUG: Added pyspark to context successfully.'
+        self.pycsv = PySpark_csv()
         self.sqlcontext = SQLContext(self.context)
 
     def get_df(self, df_name):
         # self.context.addPyFile(self.sparkcsv_python_file)
         # import pyspark_csv as pycsv
-        return csvToDataFrame(self.sqlcontext, self.context.textFile(df_name))
+        return self.pycsv.csvToDataFrame(self.sqlcontext, self.context.textFile(df_name))
 
     def workers(self):
         """Return a dictionary of the workers"""
--- raco/backends/spark/pyspark_csv.py
+++ raco/backends/spark/pyspark_csv.py
@@ -28,154 +28,155 @@ import dateutil.parser
 from pyspark.sql.types import (StringType, DoubleType, TimestampType, NullType,
                                IntegerType, StructType, StructField)
 
-py_version = sys.version_info[0]
+class PySpark_csv(object):
+    def __init__(self):
+        self.py_version = sys.version_info[0]
+
+    def csvToDataFrame(self, sqlCtx, rdd, columns=None, sep=",", parseDate=True):
+        """Converts CSV plain text RDD into SparkSQL DataFrame (former SchemaRDD)
+        using PySpark. If columns not given, assumes first row is the header.
+        If separator not given, assumes comma separated
+        """
+        if self.py_version < 3:
+            def toRow(line):
+                return self.toRowSep(line.encode('utf-8'), sep)
+        else:
+            def toRow(line):
+                return self.toRowSep(line, sep)
+
+        rdd_array = rdd.map(toRow)
+        rdd_sql = rdd_array
+
+        if columns is None:
+            columns = rdd_array.first()
+            rdd_sql = rdd_array.zipWithIndex().filter(
+                lambda r_i: r_i[1] > 0).keys()
+        column_types = self.evaluateType(rdd_sql, parseDate)
+
+        def toSqlRow(row):
+            return self.toSqlRowWithType(row, column_types)
+
+        schema = self.makeSchema(zip(columns, column_types))
+
+        return sqlCtx.createDataFrame(rdd_sql.map(toSqlRow), schema=schema)
 
 
-def csvToDataFrame(sqlCtx, rdd, columns=None, sep=",", parseDate=True):
-    """Converts CSV plain text RDD into SparkSQL DataFrame (former SchemaRDD)
-    using PySpark. If columns not given, assumes first row is the header.
-    If separator not given, assumes comma separated
-    """
-    if py_version < 3:
-        def toRow(line):
-            return toRowSep(line.encode('utf-8'), sep)
-    else:
-        def toRow(line):
-            return toRowSep(line, sep)
+    def makeSchema(self, columns):
+        struct_field_map = {'string': StringType(),
+                            'date': TimestampType(),
+                            'double': DoubleType(),
+                            'int': IntegerType(),
+                            'none': NullType()}
+        fields = [StructField(k, struct_field_map[v], True) for k, v in columns]
 
-    rdd_array = rdd.map(toRow)
-    rdd_sql = rdd_array
-
-    if columns is None:
-        columns = rdd_array.first()
-        rdd_sql = rdd_array.zipWithIndex().filter(
-            lambda r_i: r_i[1] > 0).keys()
-    column_types = evaluateType(rdd_sql, parseDate)
-
-    def toSqlRow(row):
-        return toSqlRowWithType(row, column_types)
-
-    schema = makeSchema(zip(columns, column_types))
-
-    return sqlCtx.createDataFrame(rdd_sql.map(toSqlRow), schema=schema)
+        return StructType(fields)
 
 
-def makeSchema(columns):
-    struct_field_map = {'string': StringType(),
-                        'date': TimestampType(),
-                        'double': DoubleType(),
-                        'int': IntegerType(),
-                        'none': NullType()}
-    fields = [StructField(k, struct_field_map[v], True) for k, v in columns]
-
-    return StructType(fields)
+    def toRowSep(self, line, d):
+        """Parses one row using csv reader"""
+        for r in csv.reader([line], delimiter=d):
+            return r
 
 
-def toRowSep(line, d):
-    """Parses one row using csv reader"""
-    for r in csv.reader([line], delimiter=d):
-        return r
+    def toSqlRowWithType(self, row, col_types):
+        """Convert to sql.Row"""
+        d = row
+        for col, data in enumerate(row):
+            typed = col_types[col]
+            if self.isNone(data):
+                d[col] = None
+            elif typed == 'string':
+                d[col] = data
+            elif typed == 'int':
+                d[col] = int(round(float(data)))
+            elif typed == 'double':
+                d[col] = float(data)
+            elif typed == 'date':
+                d[col] = self.toDate(data)
+        return d
 
 
-def toSqlRowWithType(row, col_types):
-    """Convert to sql.Row"""
-    d = row
-    for col, data in enumerate(row):
-        typed = col_types[col]
-        if isNone(data):
-            d[col] = None
-        elif typed == 'string':
-            d[col] = data
-        elif typed == 'int':
-            d[col] = int(round(float(data)))
-        elif typed == 'double':
-            d[col] = float(data)
-        elif typed == 'date':
-            d[col] = toDate(data)
-    return d
+    # Type converter
+    def isNone(self, d):
+        return (d is None or d == 'None' or
+                d == '?' or
+                d == '' or
+                d == 'NULL' or
+                d == 'null')
 
 
-# Type converter
-def isNone(d):
-    return (d is None or d == 'None' or
-            d == '?' or
-            d == '' or
-            d == 'NULL' or
-            d == 'null')
+    def toDate(self, d):
+        return dateutil.parser.parse(d)
 
 
-def toDate(d):
-    return dateutil.parser.parse(d)
-
-
-def getRowType(row):
-    """Infers types for each row"""
-    d = row
-    for col, data in enumerate(row):
-        try:
-            if isNone(data):
-                d[col] = 'none'
-            else:
-                num = float(data)
-                if num.is_integer():
-                    d[col] = 'int'
-                else:
-                    d[col] = 'double'
-        except:
+    def getRowType(self, row):
+        """Infers types for each row"""
+        d = row
+        for col, data in enumerate(row):
             try:
-                toDate(data)
-                d[col] = 'date'
+                if self.isNone(data):
+                    d[col] = 'none'
+                else:
+                    num = float(data)
+                    if num.is_integer():
+                        d[col] = 'int'
+                    else:
+                        d[col] = 'double'
+            except:
+                try:
+                    self.toDate(data)
+                    d[col] = 'date'
+                except:
+                    d[col] = 'string'
+        return d
+
+
+    def getRowTypeNoDate(self, row):
+        """Infers types for each row"""
+        d = row
+        for col, data in enumerate(row):
+            try:
+                if self.isNone(data):
+                    d[col] = 'none'
+                else:
+                    num = float(data)
+                    if num.is_integer():
+                        d[col] = 'int'
+                    else:
+                        d[col] = 'double'
             except:
                 d[col] = 'string'
-    return d
+        return d
 
 
-def getRowTypeNoDate(row):
-    """Infers types for each row"""
-    d = row
-    for col, data in enumerate(row):
-        try:
-            if isNone(data):
-                d[col] = 'none'
-            else:
-                num = float(data)
-                if num.is_integer():
-                    d[col] = 'int'
-                else:
-                    d[col] = 'double'
-        except:
-            d[col] = 'string'
-    return d
-
-
-def reduceTypes(a, b):
-    """Reduces column types among rows to find common denominator"""
-    type_order = {'string': 0, 'date': 1, 'double': 2, 'int': 3, 'none': 4}
-    reduce_map = {'int': {0: 'string', 1: 'string', 2: 'double'},
-                  'double': {0: 'string', 1: 'string'},
-                  'date': {0: 'string'}}
-    d = a
-    for col, a_type in enumerate(a):
-        # a_type = a[col]
-        b_type = b[col]
-        if a_type == 'none':
-            d[col] = b_type
-        elif b_type == 'none':
-            d[col] = a_type
-        else:
-            order_a = type_order[a_type]
-            order_b = type_order[b_type]
-            if order_a == order_b:
+    def reduceTypes(self, a, b):
+        """Reduces column types among rows to find common denominator"""
+        type_order = {'string': 0, 'date': 1, 'double': 2, 'int': 3, 'none': 4}
+        reduce_map = {'int': {0: 'string', 1: 'string', 2: 'double'},
+                      'double': {0: 'string', 1: 'string'},
+                      'date': {0: 'string'}}
+        d = a
+        for col, a_type in enumerate(a):
+            # a_type = a[col]
+            b_type = b[col]
+            if a_type == 'none':
+                d[col] = b_type
+            elif b_type == 'none':
                 d[col] = a_type
-            elif order_a > order_b:
-                d[col] = reduce_map[a_type][order_b]
-            elif order_a < order_b:
-                d[col] = reduce_map[b_type][order_a]
-    return d
+            else:
+                order_a = type_order[a_type]
+                order_b = type_order[b_type]
+                if order_a == order_b:
+                    d[col] = a_type
+                elif order_a > order_b:
+                    d[col] = reduce_map[a_type][order_b]
+                elif order_a < order_b:
+                    d[col] = reduce_map[b_type][order_a]
+        return d
 
 
-def evaluateType(rdd_sql, parseDate):
-    if parseDate:
-        return rdd_sql.map(getRowType).reduce(reduceTypes)
-    else:
-        return rdd_sql.map(getRowTypeNoDate).reduce(reduceTypes)
+    def evaluateType(self, rdd_sql, parseDate):
+        if parseDate:
+            return rdd_sql.map(self.getRowType).reduce(self.reduceTypes)
+        else:
+            return rdd_sql.map(self.getRowTypeNoDate).reduce(self.reduceTypes)
