1a8a116ad598c99d2d883d3b5d6a6a31560d3e1c
Brandon Myers <bdmyers@cs.washington.edu>
2016-07-08 14:21:51 -0700

    make it harder to make the tests fail from old inputs

--- c_test_environment/generate_test_relations.py
+++ c_test_environment/generate_test_relations.py
@@ -122,15 +122,11 @@ def gen_files():
 
 
 def need_generate(cpdir=''):
-    if not os.path.isfile(os.path.join(cpdir, 'test.db')):
-        return True
+    if os.path.isfile(os.path.join(cpdir, 'test.db')):
+        os.remove(os.path.join(cpdir, 'test.db'))
 
-    for n, _, _, nf in gen_files():
-        fname = get_name(n, nf)
-        if not os.path.isfile(os.path.join(cpdir, fname)):
-            return True
-
-    return False
+    # always re-generate the inputs
+    return True
 
 
 def generate_default(cpdir=None):
