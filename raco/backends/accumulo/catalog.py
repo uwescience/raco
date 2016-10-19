2348c7262df1cd1fc8d771a586928c1b7c7872cb
Dylan Hutchison <dhutchis@cs.washington.edu>
2016-10-18 21:32:14 -0700

    up

--- raco/backends/accumulo/catalog.py
+++ raco/backends/accumulo/catalog.py
@@ -19,8 +19,22 @@ class AccumuloCatalog(Catalog):
                 "no schema for relation %s because no connection" % rel_key)
 
         props = self.connection.getTableProperties(accumulo_rel_key)
-        scheme = props['table.custom.scheme']
-        return Scheme(eval(scheme))
+        schemestr = props['table.custom.scheme']
+        sch = Scheme(eval(schemestr))
+        return sch
+        # sch2 = Scheme()
+        #
+        # def unmapTypes(s, c):
+        #     name = c[0]
+        #     if c[1] != "LONG_TYPE":
+        #         _type = c[1]
+        #     else:
+        #         _type = "INT_TYPE"
+        #     s.asdict[name] = (len(s.attributes), _type)
+        #     s.attributes.append((name, _type))
+        #
+        # map(lambda c: unmapTypes(sch2, c), sch.attributes)
+        # return sch2
 
     def get_num_servers(self):
         # todo
@@ -39,10 +53,17 @@ class AccumuloCatalog(Catalog):
         props = self.connection.getTableProperties(accumulo_rel_key)
         howPartitioned = props['table.custom.howPartitioned']
 
-        return RepresentationProperties(
-            hash_partitioned=frozenset( eval(howPartitioned) ))
+        schemestr = props['table.custom.scheme']
+        sch = Scheme(eval(schemestr))
 
-#conn.setTableProperty('public_adhoc_netflow','table.custom.scheme','[("TotBytes","LONG_TYPE"),("StartTime","STRING_TYPE"),("SrcAddr","STRING_TYPE"),("DstAddr","STRING_TYPE"),("RATE","DOUBLE_TYPE"),("Dur","DOUBLE_TYPE"),("Dir","STRING_TYPE"),("Proto","STRING_TYPE"),("Sport","STRING_TYPE"),("Dport","STRING_TYPE"),("State","STRING_TYPE"),("sTos","LONG_TYPE"),("dTos","LONG_TYPE"),("TotPkts","LONG_TYPE"),("SrcBytes","LONG_TYPE"),("Label","STRING_TYPE")]')
+        hp = map(lambda s: AttIndex(sch.getPosition(s)), eval(howPartitioned))
+
+
+        return RepresentationProperties(
+            hash_partitioned=frozenset(hp))
+            # hash_partitioned=frozenset( eval(howPartitioned) ))
+
+#conn.setTableProperty('public_adhoc_netflow','table.custom.scheme','[("TotBytes","INT_TYPE"),("StartTime","STRING_TYPE"),("SrcAddr","STRING_TYPE"),("DstAddr","STRING_TYPE"),("RATE","DOUBLE_TYPE"),("Dur","DOUBLE_TYPE"),("Dir","STRING_TYPE"),("Proto","STRING_TYPE"),("Sport","STRING_TYPE"),("Dport","STRING_TYPE"),("State","STRING_TYPE"),("sTos","INT_TYPE"),("dTos","INT_TYPE"),("TotPkts","INT_TYPE"),("SrcBytes","INT_TYPE"),("Label","STRING_TYPE")]')
 #conn.setTableProperty('public_adhoc_netflow','table.custom.howPartitioned','["TotBytes", "StartTime"]')
 
 
